3b82b584d4107484e1b0cb3e400f59049b626c07
naviadev <qo2oq1020@gmail.com>
2024-08-15 20:37:05 +0900

    Feat | create_sample.command.ts : 샘플 생성 커맨드 작성

---
+++ packages/api/src/Sample/application/command/create_sample.command.ts
@@ -0,0 +1,10 @@
+import { ICommand } from '@nestjs/cqrs';
+export class CreateSampleCommand implements ICommand {
+  constructor(
+    public readonly nickname: string,
+    public readonly password: string,
+    public readonly title: string,
+    public readonly content: string,
+    public readonly tags: string[],
+  ) {}
+}
