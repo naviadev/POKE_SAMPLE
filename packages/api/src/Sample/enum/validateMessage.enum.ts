3e5afdd710ad17635834b03e784888f3a85afdcf
naviadev <qo2oq1020@gmail.com>
2024-08-15 20:36:23 +0900

    Style | ValidateMessage.enum.ts : 유효성 검사 에러 처리 메세지 상수화

---
+++ packages/api/src/Sample/enum/validateMessage.enum.ts
@@ -0,0 +1,8 @@
+export enum ValidateMessage {
+  __CONTENT_VALIDATE_ERROR = '본문 유효성 검사 실패',
+  __TAG_VALIDATE_ERROR = '태그 유효성 검사 실패',
+  __TITLE_VALIDATE_ERROR = '제목 유효성 검사 실패',
+  __SAMPLEID_VALIDATE_ERROR = '샘플 아이디 유효성 검사 실패',
+  __NICKNAME_VALIDATE_ERROR = '닉네임 유효성 검사 실패',
+  __PASSWORD_VALIDATE_ERROR = '비밀번호 유효성 검사 실패',
+}
