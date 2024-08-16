db6afa8c7ee3870df032532f30d295929e6611b1
naviadev <qo2oq1020@gmail.com>
2024-08-16 16:00:25 +0900

    Feat | sample.entity.ts : 데이터베이스 테이블 구조를 정의하는 TypeORM Entity 작성

---
+++ packages/api/src/Sample/infrastructure/entity/sample.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('sample')
+export class SampleEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  nick_name: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  title: string;
+
+  @Column('text')
+  content: string;
+
+  @Column('simple-array')
+  tags: string[];
+}
