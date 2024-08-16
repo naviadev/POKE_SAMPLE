95b26bd0f5ea2761dfd724053bdbde1b9f373157
naviadev <qo2oq1020@gmail.com>
2024-08-16 16:01:51 +0900

    Feat | content_update.event.ts : 샘플 게시글을 업데이트하는 이벤트 정의

---
+++ packages/api/src/Sample/domain/event/content_update.event.ts
@@ -0,0 +1,16 @@
+import { SampleId } from '../value-object/sampleId.vo';
+import { Title } from '../value-object/title.vo';
+import { Content } from '../value-object/content.vo';
+import { Tag } from '../value-object/tag.vo';
+import { IEvent } from '@nestjs/cqrs';
+/**
+ * 추상적인 형태로 이벤트를 정의.
+ */
+export class SampleUpdatedEvent implements IEvent {
+  constructor(
+    public readonly id: SampleId,
+    public readonly newTitle: Title,
+    public readonly newContent: Content,
+    public readonly newTags: Tag[],
+  ) {}
+}
