c866bf0ff1841176269a4089cfe1ab5fa0484efe
naviadev <qo2oq1020@gmail.com>
2024-08-16 16:03:52 +0900

    Feat | sample.ts : 이벤트 메서드 정의 apply 를 통해 이벤트 기록 및 bus 전송

--- packages/api/src/Sample/domain/entity/sample.ts
+++ packages/api/src/Sample/domain/entity/sample.ts
@@ -5,15 +5,83 @@ import { Password } from '../value-object/password.vo';
 import { Title } from '../value-object/title.vo';
 import { Content } from '../value-object/content.vo';
 import { Tag } from '../value-object/tag.vo';
+import { ValidateMessage } from 'src/Sample/enum/validateMessage.enum';
+import { SampleUpdatedEvent } from '../event/content_update.event';
+
+/**
+ * Domain
+ * * Class : Sample
+ * ? sample에 대한 기본적인 데이터 구조를 나타내며, Update 메서드를 통해 도메인 객체 자체를 업데이트 할 수 있음.
+ */
 export class Sample extends AggregateRoot {
   constructor(
     private readonly id: SampleId,
-    private nickname: Nickname,
-    private password: Password,
+    private readonly nickname: Nickname,
+    private readonly password: Password,
     private title: Title,
     private content: Content,
     private tags: Tag[],
   ) {
     super();
+  }
+
+  // Getter methods
+  getId(): SampleId {
+    return this.id;
+  }
+
+  getNickname(): Nickname {
+    return this.nickname;
+  }
+
+  getPassword(): Password {
+    return this.password;
+  }
+
+  getTitle(): Title {
+    return this.title;
+  }
+
+  getContent(): Content {
+    return this.content;
+  }
+
+  getTags(): Tag[] {
+    return this.tags;
+  }
+
+  updateSample(
+    newTitle: Title | null,
+    newContent: Content | null,
+    newTags: Tag[] | null,
+    password: Password,
+  ): void {
+    if (this.password !== password) {
+      throw new Error(ValidateMessage.__PASSWORD_MATCH_ERROR);
+    }
+
+    let updated = false;
+    if (newTitle !== null && this.title !== newTitle) {
+      this.title = newTitle;
+      updated = true;
+    }
+    if (newContent !== null && this.content !== newContent) {
+      this.content = newContent;
+      updated = true;
+    }
+    if (
+      newTags !== null &&
+      JSON.stringify(this.tags) !== JSON.stringify(newTags)
+    ) {
+      this.tags = newTags;
+      updated = true;
+    }
+
+    if (updated) {
+      // 해당 이벤트가 발생했다는 것을 어그리게이트로 알림. apply -> 어그리게이트 메서드 . 기록, 버스로 publish
+      this.apply(
+        new SampleUpdatedEvent(this.id, this.title, this.content, this.tags),
+      );
+    }
   }
 }
