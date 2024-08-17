92139350decac0147907907f4cab1e0c1551bcc3
naviadev <qo2oq1020@gmail.com>
2024-08-17 17:59:36 +0900

    Feat | get_sample.handler.ts : 모든 샘플에 대한 데이터를 가져오는 핸들러 작성
    추후 n개의 최근 샘플만 가져오는 형태로 변경할 예정.

---
+++ packages/api/src/Sample/application/query/handler/get_sample.handler.ts
@@ -0,0 +1,15 @@
+import { IQueryHandler } from '@nestjs/cqrs';
+import { ServiceMessage } from 'src/Sample/enum/serviceMessage.enum';
+import { SampleRepository } from 'src/Sample/infrastructure/repository/sample.repository';
+
+export class GetSampleHandler implements IQueryHandler<GetSampleHandler> {
+  constructor(private readonly sampleRepository: SampleRepository) {}
+  // 모든 샘플 데이터들을 가져올 것이기 떄문에 쿼리를 매개변수로 가져오는 것을 생략.
+  async execute(): Promise<any> {
+    const sample = await this.sampleRepository.findAll();
+    if (!sample) {
+      throw new Error(ServiceMessage.__CAN_NOT_FIND_SAMPLE);
+    }
+    return sample;
+  }
+}
