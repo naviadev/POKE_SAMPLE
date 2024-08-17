0674a266ea192892d4f84f0aa6e9808a14d0fedc
naviadev <qo2oq1020@gmail.com>
2024-08-17 15:01:44 +0900

    Fix | CreateSampleCommand.ts : 샘플 커맨드 수정
    Postgresql database와 동일한 컬럼값을 키값으로 할당하기 위해 nickname -> nick_name과 같은 언더바 형식의 키값으로 변경하였다

--- packages/api/src/Sample/application/command/create_sample.command.ts
+++ packages/api/src/Sample/application/command/create_sample.command.ts
@@ -1,7 +1,7 @@
 import { ICommand } from '@nestjs/cqrs';
 export class CreateSampleCommand implements ICommand {
   constructor(
-    public readonly nickname: string,
+    public readonly nick_name: string,
     public readonly password: string,
     public readonly title: string,
     public readonly content: string,
