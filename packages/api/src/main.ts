a9381554cb822f6cd5ff6ded263e4d098050719b
naviadev <qo2oq1020@gmail.com>
2024-08-18 23:05:15 +0900

    Feat | main.ts : AppDataSource Initialize 작업
    해당 모듈을 통해 트랜잭션 관리를 수행할 예정이다.

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import { AppDataSource } from 'appDataSource';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
+  await AppDataSource.initialize();
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:3000',
