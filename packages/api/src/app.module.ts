516627cbb72c6660271f506e3cf97816daa07274
naviadev <qo2oq1020@gmail.com>
2024-08-18 23:04:10 +0900

    Fix | app.module.ts : import 데이터베이스 옵션 처리 변경, 모듈 import 추가, 미들웨어 로깅 모듈 추가
    1. AppDataSource.options를 통해 데이터베이스 인스턴스.
    2. Mddileware 기능을 통해 간단한 Request,Response 응답 로깅

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ORMconfig } from 'ormConfig';
 import { SampleModule } from './Sample/sample.module';
+import { PokemonModule } from './Pokemon/pokemon.module';
+import { LoggerMiddleware } from './Shared/middleware/logger.mw';
+import { AppDataSource } from 'appDataSource';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ORMconfig), SampleModule],
+  imports: [
+    TypeOrmModule.forRoot(AppDataSource.options),
+    SampleModule,
+    PokemonModule,
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
