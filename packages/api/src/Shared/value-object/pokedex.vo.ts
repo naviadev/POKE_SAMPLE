73772e4ce9b9d37e9f4a22ec26bee2569084e3b9
naviadev <qo2oq1020@gmail.com>
2024-08-18 23:08:12 +0900

    Feat | pokedex.vo.ts : 도감번호 Vo 객체 생성
    1. Sample에서도 사용해야하는 Vo이기에, 반복적인 선언보다 Shared 에서 관리할 수 있도록 작성하였다.

---
+++ packages/api/src/Shared/value-object/pokedex.vo.ts
@@ -0,0 +1,20 @@
+import { ValidateMessage_Shared } from 'src/Shared/enum/validateMessage.shared';
+
+export class Pokedex {
+  private readonly value: number;
+  constructor(value: number) {
+    if (!this.isValid(value)) {
+      throw new Error(ValidateMessage_Shared.__POKEDEX_VALIDATE_ERROR);
+    }
+    this.value = value;
+  }
+  getValue(): number {
+    return this.value;
+  }
+  private isValid(value: number): boolean {
+    return value >= 1 && value <= 1025 ? true : false;
+  }
+  static create(value: number): Pokedex {
+    return new Pokedex(value);
+  }
+}
