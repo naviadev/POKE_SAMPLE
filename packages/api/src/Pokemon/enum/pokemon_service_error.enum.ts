d5d5155b987ca4ba17f9e25287b5bcb9d565427d
naviadev <qo2oq1020@gmail.com>
2024-08-18 23:14:48 +0900

    Style | pokemon.*_message.enum.ts : 서비스, 유효성 검사 관련 메세지 상수화

---
+++ packages/api/src/Pokemon/enum/pokemon_service_error.enum.ts
@@ -0,0 +1,4 @@
+export enum ServiceMessage_Pokemon {
+  __FAILD_SEARCH = '검색 실패',
+  __QUERY_EXEC_FAILED = '쿼리 실행 실패',
+}
---
+++ packages/api/src/Pokemon/enum/pokemon_validate_message.enum.ts
@@ -0,0 +1,3 @@
+export enum ValidateMessage_Pokemon {
+  __POKEMON_NAME_VALIDATE_ERROR = '유효하지 않은 포켓몬 이름',
+}
