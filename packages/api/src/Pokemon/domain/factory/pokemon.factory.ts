967ffa492db850f7f349018cbcf4dbd9341c8b75
naviadev <qo2oq1020@gmail.com>
2024-08-18 23:14:16 +0900

    Feat | pokemon.factory.ts : Pokemon Domain 객체를 인스턴스할 수 있는 factroy class 추가

---
+++ packages/api/src/Pokemon/domain/factory/pokemon.factory.ts
@@ -0,0 +1,21 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { EventPublisher } from '@nestjs/cqrs';
+import { Pokemon } from '../entity/pokemon';
+import { Pokedex } from 'src/Shared/value-object/pokedex.vo';
+import { PokemonName } from '../value-object/pokemon_name.vo';
+
+type PokemonOptions = Readonly<{
+  pokedex: number;
+  name: string;
+}>;
+
+@Injectable()
+export class PokemonFactory {
+  @Inject(EventPublisher) private readonly eventPublisher: EventPublisher;
+
+  create(options: PokemonOptions) {
+    return this.eventPublisher.mergeObjectContext(
+      new Pokemon(new Pokedex(options.pokedex), new PokemonName(options.name)),
+    );
+  }
+}
