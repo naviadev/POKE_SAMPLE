e317216a1d44a90ff9a19f72bfedfc66493375a9
naviadev <qo2oq1020@gmail.com>
2024-08-18 23:18:42 +0900

    Feat | create_sample.handler.ts : DataSource 모듈을 사용해 트랜잭션 관리 구문 추가
    추후 동시적인 작업이 진행될 수 있는 부분이기 때문에 트랜잭션을 통해 단위를 묶을 수 있도록 하였다.

--- packages/api/src/Sample/application/command/handler/create_sample.handler.ts
+++ packages/api/src/Sample/application/command/handler/create_sample.handler.ts
@@ -3,8 +3,9 @@ import { CreateSampleCommand } from '../create_sample.command';
 
 import { SampleFactory } from 'src/Sample/domain/factory/sample.factory';
 import { SampleRepository } from 'src/Sample/infrastructure/repository/sample.repository';
-import { Connection } from 'typeorm';
+import { DataSource, QueryRunner } from 'typeorm';
 import { ServiceMessage } from 'src/Sample/enum/serviceMessage.enum';
+import { InjectDataSource } from '@nestjs/typeorm';
 
 /**
  * * Class : CreateSampleCommandHandler
@@ -16,16 +17,19 @@ import { ServiceMessage } from 'src/Sample/enum/serviceMessage.enum';
 export class CreateSampleCommandHandler
   implements ICommandHandler<CreateSampleCommand>
 {
+  private readonly queryRunner: QueryRunner;
+
   constructor(
     private readonly sampleFactory: SampleFactory, // 도메인 생성을 위한 팩토리 .
     private readonly sampleRepository: SampleRepository, // 도메인 저장을 위한 레포지토리 .
-    private readonly connection: Connection, // 트랜잭션 관리 모듈 주입.
-  ) {}
+    @InjectDataSource() private readonly dataSource: DataSource, // 트랜잭션 관리 모듈 주입.
+  ) {
+    this.queryRunner = this.dataSource.createQueryRunner(); // QueryRunner 생성
+  }
   async execute(command: CreateSampleCommand): Promise<any> {
     const { nick_name, password, pokedex, title, content, tags } = command; // Comand 구조 분해 .
-    const queryRunner = this.connection.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction(); //트랜잭션 시작.
+    await this.queryRunner.connect();
+    await this.queryRunner.startTransaction();
     try {
       // Factory 객체를 통해 Sample 도메인 인스턴스.
       const sample = this.sampleFactory.create({
@@ -37,12 +41,12 @@ export class CreateSampleCommandHandler
         tags,
       });
       await this.sampleRepository.save(sample); // 커스텀 Repository를 통해 Database 쓰기 작업 수행
-      await queryRunner.commitTransaction(); // 트랜잭션 커밋.
+      await this.queryRunner.commitTransaction(); // 트랜잭션 커밋.
     } catch (error) {
       console.error(ServiceMessage.__COMMAND_EXEC_FAILED, error);
       throw error;
     } finally {
-      await queryRunner.release(); // 트랜잭션 해제
+      await this.queryRunner.release(); // 트랜잭션 해제
     }
   }
 }
