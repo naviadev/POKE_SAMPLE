625bff493d387c137c2664b406634a4fd3ed1f56
naviadev <qo2oq1020@gmail.com>
2024-08-26 15:44:25 +0900

    Fix | pokemonSearchForm.tsx : onChange를 통해 상태변수를 전환할 수 있도록 변경

--- packages/client/src/components/molecule_extends/pokemonSearch/pokemonSearchForm.tsx
+++ packages/client/src/components/molecule_extends/pokemonSearch/pokemonSearchForm.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import useSearchPokemonOptions from "./hooks/useSampleCard";
 import SelectField from "@/components/molecule/selectField/selectField";
+import Option from "../../../../common/interface/option.interface";
 
-const SearchPokemonForm: React.FC = () => {
+interface SearchPokemonFormProps {
+  onPokemonChange: (pokmon: Option | null) => void;
+}
+
+const SearchPokemonForm: React.FC<SearchPokemonFormProps> = ({
+  onPokemonChange,
+}) => {
   const { pokemonOptionList, isLoading, loadOptions } =
     useSearchPokemonOptions();
 
@@ -12,10 +19,11 @@ const SearchPokemonForm: React.FC = () => {
       isClearable
       isLoading={isLoading}
       onInputChange={loadOptions} // 비동기 검색어 처리 함수 전달
+      onChange={onPokemonChange}
       options={pokemonOptionList}
       placeholder="포켓몬을 검색해주세요"
       styles={{ menu: (provided: any) => ({ ...provided, zIndex: 9999 }) }}
-      label="Pokedex"
+      label="Pokemon"
     />
   );
 };
