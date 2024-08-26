80102502ac186883476679dae9210528b42e1833
naviadev <qo2oq1020@gmail.com>
2024-08-26 15:43:27 +0900

    Fix | selectField.tsx : options 옵셔널 추가, onChange 구문 추가

--- packages/client/src/components/molecule/selectField/selectField.tsx
+++ packages/client/src/components/molecule/selectField/selectField.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import Select from "react-select";
 import { OptionsOrGroups } from "react-select";
-
-interface Options {
-  value: number;
-  label: string;
-}
-
+import Option from "../../../../common/interface/option.interface";
 interface SelectProps {
   id?: string;
   label?: string; // Label 추가
   isClearable?: boolean;
   isLoading?: boolean;
   onInputChange?: (inputValue: string) => void;
-  options: Options[];
+  onChange?: (selectedOption: any) => void;
+  options?: Option[];
   placeholder?: string;
   styles?: object;
+  onFocus?: () => void; // 추가된 프로퍼티
 }
 
 const SelectField: React.FC<SelectProps> = ({
@@ -24,6 +21,8 @@ const SelectField: React.FC<SelectProps> = ({
   isClearable = false,
   isLoading = false,
   onInputChange,
+  onChange,
+  onFocus,
   options,
   placeholder = "Select...",
   styles = {},
@@ -31,6 +30,11 @@ const SelectField: React.FC<SelectProps> = ({
   const handleInputChange = (inputValue: string) => {
     if (onInputChange) {
       onInputChange(inputValue);
+    }
+  };
+  const handleChange = (selectedOption: any) => {
+    if (onChange) {
+      onChange(selectedOption);
     }
   };
 
@@ -41,9 +45,11 @@ const SelectField: React.FC<SelectProps> = ({
         id={id}
         isClearable={isClearable}
         isLoading={isLoading}
-        onInputChange={handleInputChange} 
+        onInputChange={handleInputChange}
+        onChange={handleChange}
         options={options}
         placeholder={placeholder}
+        onFocus={onFocus}
         styles={styles}
       />
     </div>
