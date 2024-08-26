1cb4f871842111dc172eb7b8229ffc3368ae3c01
naviadev <qo2oq1020@gmail.com>
2024-08-26 17:41:43 +0900

    Fix | pokemon_name.vo.ts: 유효성 검사 문제로 임시적인 조치
    1. 타입 : 널
    2. 폴리곤 Z 와 같이 한글 이외 특수문자, 알파벳등을 포함하는 포켓몬이 존재하여 이를 해결하기 위해 조치 .

--- packages/api/src/Pokemon/domain/value-object/pokemon_name.vo.ts
+++ packages/api/src/Pokemon/domain/value-object/pokemon_name.vo.ts
@@ -1,11 +1,11 @@
-import { ValidateMessage_Pokemon } from 'src/Pokemon/enum/pokemon_validate_message.enum';
+// import { ValidateMessage_Pokemon } from 'src/Pokemon/enum/pokemon_validate_message.enum';
 
 export class PokemonName {
   private readonly value: string;
   constructor(value: string) {
-    if (!this.isValid(value)) {
-      throw new Error(ValidateMessage_Pokemon.__POKEMON_NAME_VALIDATE_ERROR);
-    }
+    // if (!this.isValid(value)) {
+    //   throw new Error(ValidateMessage_Pokemon.__POKEMON_NAME_VALIDATE_ERROR);
+    // }
     this.value = value;
   }
   getValue(): string {
@@ -13,7 +13,7 @@ export class PokemonName {
   }
   private isValid(value: string): boolean {
     const koreanRegex = /^[가-힣]+$/;
-    return koreanRegex.test(value);
+    return value.length > 0 && koreanRegex.test(value);
   }
   static create(value: string): PokemonName {
     return new PokemonName(value);
