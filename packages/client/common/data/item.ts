ef6d5d7e44316fd1da4227afd6229a7984b0a4f9
naviadev <qo2oq1020@gmail.com>
2024-08-26 17:42:50 +0900

    Feat | item.ts: 배틀 아이템 Client Data에 포함

---
+++ packages/client/common/data/item.ts
@@ -0,0 +1,79 @@
+import Option from "../interface/option.interface";
+
+const item: Option[] = [
+  { label: '광각렌즈', value: 0 }, // Wide Lens
+  { label: '달인의띠', value: 1 }, // Expert Belt
+  { label: '메트로놈', value: 2 }, // Metronome
+  { label: '박식안경', value: 3 }, // Wise Glasses
+  { label: '생명의구슬', value: 4 }, // Life Orb
+  { label: '초점렌즈', value: 5 }, // Scope Lens
+  { label: '파괴의유전자', value: 6 }, // Berserk Gene
+  { label: '포커스렌즈', value: 7 }, // Zoom Lens
+  { label: '힘의머리띠', value: 8 }, // Muscle Band
+  { label: '뜨거운바위', value: 9 }, // Heat Rock
+  { label: '보송보송바위', value: 10 }, // Smooth Rock
+  { label: '차가운바위', value: 11 }, // Icy Rock
+  { label: '축축한바위', value: 12 }, // Damp Rock
+  { label: '빛의점토', value: 13 }, // Light Clay
+  { label: '끈기갈고리손톱', value: 14 }, // Grip Claw
+  { label: '조임밴드', value: 15 }, // Binding Band
+  { label: '속임수주사위', value: 16 }, // Loaded Dice
+  { label: '구애머리띠', value: 17 }, // Choice Band
+  { label: '구애스카프', value: 18 }, // Choice Scarf
+  { label: '구애안경', value: 19 }, // Choice Specs
+  { label: '기합의띠', value: 20 }, // Focus Sash
+  { label: '기합의머리띠', value: 21 }, // Focus Band
+  { label: '맹독구슬', value: 22 }, // Toxic Orb
+  { label: '화염구슬', value: 23 }, // Flame Orb
+  { label: '끈적끈적바늘', value: 24 }, // Sticky Barb
+  { label: '검은철구', value: 25 }, // Iron Ball
+  { label: '겨냥표적', value: 26 }, // Ring Target
+  { label: '아름다운허물', value: 27 }, // Shed Shell
+  { label: '연막탄', value: 28 }, // Smoke Ball
+  { label: '탈출버튼', value: 29 }, // Eject Button
+  { label: '탈출팩', value: 30 }, // Eject Pack
+  { label: '검은오물', value: 31 }, // Black Sludge
+  { label: '먹다남은음식', value: 32 }, // Leftovers
+  { label: '조개껍질방울', value: 33 }, // Shell Bell
+  { label: '큰뿌리', value: 34 }, // Big Root
+  { label: '느림보꼬리', value: 35 }, // Lagging Tail
+  { label: '선제공격손톱', value: 36 }, // Quick Claw
+  { label: '멘탈허브', value: 37 }, // Mental Herb
+  { label: '파워풀허브', value: 38 }, // Power Herb
+  { label: '하양허브', value: 39 }, // White Herb
+  { label: '흉내허브', value: 40 }, // Mirror Herb
+  { label: '그래스시드', value: 41 }, // Grassy Seed
+  { label: '미스트시드', value: 42 }, // Misty Seed
+  { label: '사이코시드', value: 43 }, // Psychic Seed
+  { label: '일렉트릭시드', value: 44 }, // Electric Seed
+  { label: '반짝가루', value: 45 }, // Bright Powder
+  { label: '부적금화', value: 46 }, // Amulet Coin
+  { label: '빨간실', value: 47 }, // Destiny Knot
+  { label: '가벼운돌', value: 48 }, // Float Stone
+  { label: '울퉁불퉁멧', value: 49 }, // Rocky Helmet
+  { label: '레드카드', value: 50 }, // Red Card
+  { label: '풍선', value: 51 }, // Air Balloon
+  { label: '방진고글', value: 52 }, // Safety Goggles
+  { label: '구근', value: 53 }, // Absorb Bulb
+  { label: '충전지', value: 54 }, // Cell Battery
+  { label: '눈덩이', value: 55 }, // Snowball
+  { label: '돌격조끼', value: 56 }, // Assault Vest
+  { label: '빛이끼', value: 57 }, // Luminous Moss
+  { label: '약점보험', value: 58 }, // Weakness Policy
+  { label: '그라운드코트', value: 59 }, // Terrain Extender
+  { label: '방호패드', value: 60 }, // Protective Pads
+  { label: '주눅구슬', value: 61 }, // Adrenaline Orb
+  { label: '만능우산', value: 62 }, // Utility Umbrella
+  { label: '목스프레이', value: 63 }, // Throat Spray
+  { label: '통굽부츠', value: 64 }, // Heavy-Duty Boots
+  { label: '허탕보험', value: 65 }, // Blunder Policy
+  { label: '룸서비스', value: 66 }, // Room Service
+  { label: '펀치글러브', value: 67 }, // Punching Glove
+  { label: '특성가드', value: 68 }, // Ability Shield
+  { label: '클리어참', value: 69 }, // Clear Amulet
+  { label: '은밀망토', value: 70 }, // Covert Cloak
+  { label: '부스트에너지', value: 71 }, // Booster Energy
+  {label : '진화의휘석', value:72}
+];
+
+export default item;
