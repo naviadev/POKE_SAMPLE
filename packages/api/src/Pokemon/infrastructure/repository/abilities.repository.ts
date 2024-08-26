d7b38a7d87c985f00cde7b4cca6574f144b7556b
naviadev <qo2oq1020@gmail.com>
2024-08-26 15:42:39 +0900

    Feat | abilities.repository.ts : 추가된 Entity를 컨트롤하는 Repository 추가

---
+++ packages/api/src/Pokemon/infrastructure/repository/abilities.repository.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PokemonAbilitiesEntity } from '../entity/abilities.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class AbilitiesRepository {
+  constructor(
+    @InjectRepository(PokemonAbilitiesEntity)
+    private readonly abilitiesRepository: Repository<PokemonAbilitiesEntity>,
+  ) {}
+
+  async getAbilities(pokedex: number) {
+    const entity = await this.abilitiesRepository.findOne({
+      where: { pokedex: pokedex },
+    });
+    return entity;
+  }
+}
