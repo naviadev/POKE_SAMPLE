9335eb68a65828c7ad1ee8c11bc228503341fa35
naviadev <qo2oq1020@gmail.com>
2024-08-27 12:22:48 +0900

    Chore | evForm,ivForm.tsx : 경로 변경

---
+++ packages/client/src/components/molecule_extends/sampleCard/stats/evField/evForm.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import EvsField from "@/components/molecule/formField/evField";
+import { Stats } from "../../../../../../common/data/stats";
+import { Label } from "@/components/atom/shad/label";
+import StatFormProps from "../props/statsField.props";
+import {StatsFields , labelText} from "../data/label";
+
+const EvsForm: React.FC<StatFormProps> = ({ value, onChange }) => {
+
+  // Handle Max and Min actions
+  const handleMax = (field: keyof Stats) => () => {
+    const total = Object.values(value).reduce((acc, curr) => acc + curr, 0);
+    const max = 510 - (total - value[field]);
+    onChange(field, Math.min(252, max));
+  };
+
+  const handleMin = (field: keyof Stats) => () => {
+    onChange(field, 0);
+  };
+
+  const handleChange = (field: keyof Stats, newValue: number) => {
+    const total = Object.values(value).reduce((acc, curr) => acc + curr, 0);
+    const newTotal = total - value[field] + newValue;
+
+    if (newTotal <= 510) {
+      onChange(field, newValue);
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center gap-2 bg-gray-100 p-4 border border-gray-100 rounded-lg">
+      <Label>Evs - 노력치</Label>
+      <div className="grid grid-cols-2 gap-4 justify-items-center">
+        {StatsFields.map((field, index) => (
+          <EvsField
+            key={field.label}
+            id={field.label}
+            type="number"
+            label={labelText[index]}
+            value={value[field.label]}
+            onChange={(e) => handleChange(field.label, Number(e.target.value))}
+            onMax={handleMax(field.label)}
+            onMin={handleMin(field.label)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EvsForm;
---
+++ packages/client/src/components/molecule_extends/sampleCard/stats/ivField/ivForm.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import IvField from "@/components/molecule/formField/ivField";
+import { Stats } from "../../../../../../common/data/stats";
+import { Label } from "@/components/atom/shad/label";
+import StatFormProps from "../props/statsField.props";
+import { StatsFields, labelText, fieldToZ } from "../data/label";
+import determineIvGrade from "../service/grade";
+
+const IvForm: React.FC<StatFormProps> = ({ value, onChange }) => {
+  const handleMax = (field: keyof Stats) => () => {
+    onChange(field, 31);
+  };
+  const handleMin = (field: keyof Stats) => () => {
+    onChange(field, 0);
+  };
+  const totalIvValue = Object.values(value).reduce((sum, val) => sum + val, 0);
+  const zeroFields = StatsFields.filter((field) => value[field.label] === 0)
+    .map((field) => fieldToZ[field.label] || "")
+    .join(" ");
+  const ivGrade = determineIvGrade(totalIvValue);
+  const ivMessage = zeroFields ? `${ivGrade} (${zeroFields})` : ivGrade;
+
+  return (
+    <div className="flex flex-col justify-center gap-2 bg-gray-100 p-4 border border-gray-100 rounded-lg">
+      <Label>Ivs - 개체값</Label>
+      <Label className="text-gray-700 text-green-800">현재 : {ivMessage}</Label>
+      <div className="grid grid-cols-3 gap-4 justify-items-center">
+        {StatsFields.map((field, index) => (
+          <IvField
+            key={field.label}
+            id={field.label}
+            type="number"
+            label={labelText[index]}
+            value={value[field.label]}
+            onChange={(e) => onChange(field.label, Number(e.target.value))}
+            onMax={handleMax(field.label)}
+            onMin={handleMin(field.label)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default IvForm;
