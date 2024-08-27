83acf57c1f6b9900eb384c5cabcb8d51caa58617
naviadev <qo2oq1020@gmail.com>
2024-08-27 11:25:40 +0900

    Fix | formField.tsx : label -> Label 변경, props 프로퍼티 타입 변경
    value : string
    => value : string|number

--- packages/client/src/components/molecule/formField/formField.tsx
+++ packages/client/src/components/molecule/formField/formField.tsx
@@ -1,11 +1,12 @@
 import { Input } from "@/components/atom/shad/input";
+import { Label } from "@/components/atom/shad/label";
 import { ChangeEventHandler } from "react";
 
 export interface FormFieldProps {
   id: string;
   type: string;
   label: string;
-  value: string;
+  value: string | number;
   onChange: ChangeEventHandler<HTMLInputElement>;
 }
 const FormField: React.FC<FormFieldProps> = ({
@@ -17,7 +18,7 @@ const FormField: React.FC<FormFieldProps> = ({
 }) => {
   return (
     <div>
-      <label htmlFor={id}>{label}</label>
+      <Label htmlFor={id}>{label}</Label>
       <Input id={id} type={type} value={value} onChange={onChange}></Input>
     </div>
   );
