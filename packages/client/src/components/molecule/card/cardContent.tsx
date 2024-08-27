48abdd6e20e5ab8d51fc28a1175118ce5c9744fc
naviadev <qo2oq1020@gmail.com>
2024-08-27 11:23:17 +0900

    Fix | cardContent.tsx : Props 프로퍼티 추가 -> className

--- packages/client/src/components/molecule/card/cardContent.tsx
+++ packages/client/src/components/molecule/card/cardContent.tsx
@@ -1,8 +1,9 @@
 import { CardContent } from "@/components/atom/shad/card";
 interface PostCardProps {
   children: React.ReactNode;
+  className: string;
 }
-const CardContentView: React.FC<PostCardProps> = ({ children }) => {
-  return <CardContent>{children}</CardContent>;
+const CardContentView: React.FC<PostCardProps> = ({ children, className }) => {
+  return <CardContent className={className}>{children}</CardContent>;
 };
 export default CardContentView;
