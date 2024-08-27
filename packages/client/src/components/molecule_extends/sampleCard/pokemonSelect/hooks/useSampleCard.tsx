d97fc35136c1eb0c9cbdcbc533a478e41f23083e
naviadev <qo2oq1020@gmail.com>
2024-08-27 12:21:56 +0900

    Chore | useSampleCard.tsx : 경로 변경 - > hooks

---
+++ packages/client/src/components/molecule_extends/sampleCard/pokemonSelect/hooks/useSampleCard.tsx
@@ -0,0 +1,53 @@
+import { useState, useRef, useCallback } from "react";
+import debounce from "lodash.debounce";
+
+import { Pokemon } from "../../../../../../common/interface/pokemon.interface";
+import Option from "../../../../../../common/interface/option.interface";
+
+/**
+ * @pokemonOptionList : 포켓몬 검색 리스트. Select 컴포넌트에 출력될 값들을 나타낸다.
+ * @isLoading : 로딩 처리 관리.
+ * @cache : 반복적인 데이터 요청을 방지하기 위해 캐싱.
+ */
+const useSearchPokemonOptions = () => {
+  const [pokemonOptionList, setPokemonOptionList] = useState<Option[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const cache = useRef<{ [key: string]: Option[] }>({});
+
+  const loadOptions = useCallback(
+    debounce(async (inputValue: string) => {
+      if (!inputValue) return;
+      if (cache.current[inputValue]) {
+        setPokemonOptionList(cache.current[inputValue]);
+        return;
+      }
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `http://localhost:3001/pokemon/query/search/${inputValue}`
+        );
+
+        const data: Pokemon[] = await response.json();
+
+        const formattedOptions = data.map((pokemon) => ({
+          value: pokemon.pokedex,
+          label: pokemon.name,
+        }));
+
+        cache.current[inputValue] = formattedOptions;
+        console.log(formattedOptions);
+
+        setPokemonOptionList(formattedOptions);
+      } catch (error) {
+        console.error("검색 실패:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }, 300),
+    []
+  );
+
+  return { pokemonOptionList, isLoading, loadOptions };
+};
+
+export default useSearchPokemonOptions;
