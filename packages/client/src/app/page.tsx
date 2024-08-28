efc94e65b2d48cd3823f0803e1c8a615d76c3395
naviadev <qo2oq1020@gmail.com>
2024-08-28 11:38:56 +0900

    Fix | page.tsx : 기존 아토믹패턴의 적용이 되지 않은 page.tsx 코드 수정

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,59 +1,11 @@
 "use client";
-import Select from 'react-select';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
-import usePokemonOptions from '../custom_component/sample/hooks/useSampleCard';
-
-const Home: React.FC = () => {
-  const { pokemonOptionList, isLoading, loadOptions } = usePokemonOptions();
+import PostSampleCard from "@/components/organism/postSampleCard/postSampleCard";
+const POST: React.FC = () => {
   return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>샘플 작성</CardTitle>
-        <CardDescription>테스트용 샘플 작성 컴포넌트</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Title</Label>
-              <Input id="name" placeholder="제목" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="pokedex">Pokedex</Label>
-              <Select
-                id="pokedex"
-                isClearable
-                isLoading={isLoading}
-                onInputChange={loadOptions}
-                options={pokemonOptionList}
-                placeholder="도감번호"
-                styles={{ menu: (provided) => ({ ...provided, zIndex: 9999 }) }}
-              />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="content">Content</Label>
-              <Textarea className="h-80 resize-none" placeholder="내용" />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">취소</Button>
-        <Button>작성</Button>
-      </CardFooter>
-    </Card>
+    <div className="flex justify-center items-center min-h-screen bg-gray-600">
+      <PostSampleCard></PostSampleCard>
+    </div>
   );
 };
 
-export default Home;
+export default POST;
