4b85289450de8572fa0727943531e0518d173759
naviadev <qo2oq1020@gmail.com>
2024-08-29 17:15:04 +0900

    Fix | cardOverView.tsx: 컴포넌트에 커스텀 css  를 추가할 수 있도록 className을 Props에 추가

--- packages/client/src/components/molecule/card/cardOverView.tsx
+++ packages/client/src/components/molecule/card/cardOverView.tsx
@@ -3,13 +3,15 @@ import { CardDescription, CardHeader, CardTitle } from "../../atom/shad/card";
 interface CardHeaderProps {
   title: string;
   description: string;
+  className?: string;
 }
 const CardTitleOverView: React.FC<CardHeaderProps> = ({
   title,
   description,
+  className,
 }) => {
   return (
-    <CardHeader>
+    <CardHeader className={className}>
       <CardTitle>{title}</CardTitle>
       <CardDescription>{description}</CardDescription>
     </CardHeader>
