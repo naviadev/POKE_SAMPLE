076a9647ac88d9934062be557d9ec8203081ce74
naviadev <qo2oq1020@gmail.com>
2024-08-29 11:29:25 +0900

    Refactor | sampleCard.tsx , useSampleCard.tsx : 기존 모든 샘플값들을 useState로 관리하는 형식에서 Context, Reducer를 통해 관리하는 형식으로 변경

---
+++ packages/client/src/components/organism/postSampleCard/hooks/useSampleCard.tsx
@@ -0,0 +1,78 @@
+import { createContext, ReactNode, useContext, useReducer } from "react";
+import SampleCardState from "../../../../../common/interface/sampleCard.interface";
+import Option from "../../../../../common/interface/option.interface";
+
+type Action =
+  | { type: "SET_TITLE"; payload: string }
+  | { type: "SET_POKEMON"; payload: Option | null }
+  | { type: "SET_ABILITY"; payload: string | null }
+  | { type: "SET_ITEM"; payload: Option | null }
+  | { type: "SET_CONTENT"; payload: string }
+  | { type: "SET_TERA"; payload: string }
+  | { type: "SET_IV_STAT"; payload: { field: string; value: number } }
+  | { type: "SET_EV_STAT"; payload: { field: string; value: number } };
+
+const SampleCardContext = createContext<
+  { state: SampleCardState; dispatch: React.Dispatch<Action> } | undefined
+>(undefined);
+
+const SampleCardReducer = (
+  state: SampleCardState,
+  action: Action
+): SampleCardState => {
+  switch (action.type) {
+    case "SET_TITLE":
+      return { ...state, title: action.payload };
+    case "SET_POKEMON":
+      return { ...state, pokemon: action.payload };
+    case "SET_ABILITY":
+      return { ...state, ability: action.payload };
+    case "SET_ITEM":
+      return { ...state, item: action.payload };
+    case "SET_CONTENT":
+      return { ...state, content: action.payload };
+    case "SET_TERA":
+      return { ...state, tera: action.payload };
+    case "SET_IV_STAT":
+      return {
+        ...state,
+        ivs: { ...state.ivs, [action.payload.field]: action.payload.value },
+      };
+    case "SET_EV_STAT":
+      return {
+        ...state,
+        evs: { ...state.evs, [action.payload.field]: action.payload.value },
+      };
+    default:
+      return state;
+  }
+};
+
+export const SampleCardProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [state, dispatch] = useReducer(SampleCardReducer, {
+    title: "",
+    pokemon: null,
+    ability: null,
+    item: null,
+    content: "",
+    tera: "",
+    ivs: { H: 31, A: 31, B: 31, C: 31, D: 31, S: 31 },
+    evs: { H: 0, A: 0, B: 0, C: 0, D: 0, S: 0 },
+  });
+
+  return (
+    <SampleCardContext.Provider value={{ state, dispatch }}>
+      {children}
+    </SampleCardContext.Provider>
+  );
+};
+
+export const useSampleCard = () => {
+  const context = useContext(SampleCardContext);
+  if (context === undefined) {
+    throw new Error("useSampleCard must be used within a SampleCardProvider");
+  }
+  return context;
+};
---
+++ packages/client/src/components/organism/postSampleCard/sampleCard.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Image from "next/image";
+import { Card } from "../../atom/shad/card";
+import CardContentView from "../../molecule/card/cardContent";
+import CardFooterView from "../../molecule/card/cardFooter";
+import CardTitleOverView from "../../molecule/card/cardOverView";
+import TitleFormField from "../../molecule_extends/titleFormField";
+import SearchPokemonForm from "../../molecule_extends/sampleCard/pokemonSelect/pokemonSearchForm";
+import AbilitySearch from "../../molecule_extends/sampleCard/abilitiesSelect/abilitySearch";
+import ItemSelect from "../../molecule_extends/sampleCard/itemSelect/itemSelect";
+import IvForm from "../../molecule_extends/sampleCard/stats/ivField/ivForm";
+import EvsForm from "@/components/molecule_extends/sampleCard/stats/evField/evForm";
+import MoveSelect from "../../molecule_extends/sampleCard/moveSelect/moveSelect";
+import ContentArea from "../../molecule_extends/contentArea/contentArea";
+import TeraTypeSelect from "@/components/molecule_extends/sampleCard/teraSelect/teraSelect";
+import { useSampleCard, SampleCardProvider } from "./hooks/useSampleCard";
+
+const PostSampleCard: React.FC = () => {
+  const { state, dispatch } = useSampleCard();
+
+  const handleStatChange = (field: string, value: number) => {
+    dispatch({ type: "SET_IV_STAT", payload: { field, value } });
+  };
+
+  const handleEvStatChange = (field: string, value: number) => {
+    dispatch({ type: "SET_EV_STAT", payload: { field, value } });
+  };
+
+  return (
+    <Card className="w-[650px]">
+      <div className="grid grid-cols-2 item-center">
+        <CardTitleOverView
+          title="샘플 작성"
+          description="현재 시즌 : 레귤레이션 D"
+        />
+      </div>
+
+      <CardContentView className="space-y-5">
+        <TitleFormField
+          value={state.title}
+          onChange={(value: string) =>
+            dispatch({ type: "SET_TITLE", payload: value })
+          }
+        />
+        <div className="grid grid-cols-2 gap-4">
+          <SearchPokemonForm
+            onPokemonChange={(pokemon) =>
+              dispatch({ type: "SET_POKEMON", payload: pokemon })
+            }
+          />
+          <TeraTypeSelect
+            selectedType={state.tera}
+            onTypeChange={(tera) =>
+              dispatch({ type: "SET_TERA", payload: tera })
+            }
+          />
+        </div>
+        <AbilitySearch
+          pokemon={state.pokemon}
+          onAbilityChange={(ability) =>
+            dispatch({ type: "SET_ABILITY", payload: ability })
+          }
+        />
+        <ItemSelect
+          onItemChange={(item) => dispatch({ type: "SET_ITEM", payload: item })}
+        />
+        <IvForm value={state.ivs} onChange={handleStatChange} />
+        <EvsForm value={state.evs} onChange={handleEvStatChange} />
+        <MoveSelect />
+        <ContentArea
+          value={state.content}
+          onChange={(e) =>
+            dispatch({ type: "SET_CONTENT", payload: e.target.value })
+          }
+        />
+      </CardContentView>
+      <CardFooterView approveName="작성" cancelName="취소" />
+    </Card>
+  );
+};
+
+const WrappedPostSampleCard: React.FC = () => (
+  <SampleCardProvider>
+    <PostSampleCard />
+  </SampleCardProvider>
+);
+
+export default WrappedPostSampleCard;
