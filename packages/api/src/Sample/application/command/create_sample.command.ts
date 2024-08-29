dc120bc49010b7853e5f97764fb3d86b45854650
naviadev <qo2oq1020@gmail.com>
2024-08-29 17:07:41 +0900

    Fix | create_sample.command.ts : DTO 변경으로 Command 클래스 구조 변경

--- packages/api/src/Sample/application/command/create_sample.command.ts
+++ packages/api/src/Sample/application/command/create_sample.command.ts
@@ -1,13 +1,32 @@
 import { ICommand } from '@nestjs/cqrs';
+import { CreateSampleDTO } from 'src/Sample/presentation/dto/create_sample.dto';
 
 // CreateSampleCommand : 샘플 게시글 작성 커맨드
 export class CreateSampleCommand implements ICommand {
   constructor(
-    public readonly nick_name: string,
-    public readonly password: string,
     public readonly pokedex: number,
     public readonly title: string,
+    public readonly ability: string,
+    public readonly item: string,
     public readonly content: string,
-    public readonly tags: string[],
+    public readonly tera: string,
+    public readonly evs: string,
+    public readonly ivs: string,
+    public readonly id: string,
+    public readonly password: string,
   ) {}
+  static createCommand(dto: CreateSampleDTO) {
+    return new CreateSampleCommand(
+      dto.pokedex,
+      dto.title,
+      dto.ability,
+      dto.item,
+      dto.content,
+      dto.tera,
+      dto.evs,
+      dto.ivs,
+      dto.id,
+      dto.password,
+    );
+  }
 }
