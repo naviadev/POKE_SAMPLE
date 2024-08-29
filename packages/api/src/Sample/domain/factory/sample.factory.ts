8246fa743bf9d7c74b011dd0800c0ff13cde36de
naviadev <qo2oq1020@gmail.com>
2024-08-29 17:09:48 +0900

    Fix | sample.factory.ts : 팩토리 클래스 변경

--- packages/api/src/Sample/domain/factory/sample.factory.ts
+++ packages/api/src/Sample/domain/factory/sample.factory.ts
@@ -1,37 +1,57 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { EventPublisher } from '@nestjs/cqrs';
 import { Sample } from '../entity/sample';
-import { Nickname } from '../value-object/nickname.vo';
+import { Id } from '../value-object/id.vo';
 import { Password } from '../value-object/password.vo';
 import { Title } from '../value-object/title.vo';
 import { Content } from '../value-object/content.vo';
-import { Tag } from '../value-object/tag.vo';
-import { Pokedex } from 'src/Shared/value-object/pokedex.vo';
+import { Pokedex } from '../value-object/pokedex.vo';
+import { Ability } from '../value-object/ability.vo';
+import { Item } from '../value-object/item.vo';
+import { Tera } from '../value-object/tera.vo';
+import { IVs } from '../value-object/ivs.vo';
+import { EVs } from '../value-object/evs.vo';
 
 type CreateSampleOptions = Readonly<{
-  id?: string;
-  nick_name: string;
+  index?: string;
+  id: string;
   password: string;
   pokedex: number;
   title: string;
   content: string;
-  tags: string[];
+  ability: string;
+  item: string;
+  tera: string;
+  ivs: string;
+  evs: string;
 }>;
 
 @Injectable()
 export class SampleFactory {
   @Inject(EventPublisher) private readonly eventPublisher: EventPublisher;
+
   create(options: CreateSampleOptions): Sample {
-    return this.eventPublisher.mergeObjectContext(
-      new Sample(
-        null,
-        Nickname.create(options.nick_name),
-        Password.create(options.password),
-        Pokedex.create(options.pokedex),
-        Title.create(options.title),
-        Content.create(options.content),
-        Tag.create(options.tags),
-      ),
+    const sample = new Sample(
+      null, // SampleId는 null로 시작함 (자동 생성됨)
+      Id.create(options.id),
+      Password.create(options.password),
+      Pokedex.create(options.pokedex),
+      Title.create(options.title),
+      Content.create(options.content),
+      Ability.create(options.ability),
+      Item.create(options.item),
+      Tera.create(options.tera),
+      IVs.create(options.ivs),
+      EVs.create(options.evs),
     );
+
+    // EventPublisher로 객체를 래핑하여 이벤트를 발행할 수 있도록 설정
+    const sampleWithEventPublisher =
+      this.eventPublisher.mergeObjectContext(sample);
+
+    // 필요한 초기 이벤트가 있다면 발행 가능
+    // sampleWithEventPublisher.apply(new SampleCreatedEvent(sampleWithEventPublisher));
+
+    return sampleWithEventPublisher;
   }
 }
