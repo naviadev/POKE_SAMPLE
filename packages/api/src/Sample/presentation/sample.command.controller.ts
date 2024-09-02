5eeae7bad8f4866418266fd487f3ff73164529cf
naviadev <qo2oq1020@gmail.com>
2024-09-02 20:25:13 +0900

    Style | sample.command.controller.ts : 주석 추가, import 구문 디렉토리명 변경 (대문자 => 소문자)

--- packages/api/src/Sample/presentation/sample.command.controller.ts
+++ packages/api/src/Sample/presentation/sample.command.controller.ts
@@ -6,46 +6,38 @@ import {
   HttpStatus,
   Post,
 } from '@nestjs/common';
-import { CreateSampleCommandHandler } from '../application/command/handler/create_sample.handler';
+import { CreateSampleCommandHandler } from '../application/command/handler/createSample.handler';
 import { CreateSampleDTO } from './dto/create_sample.dto';
-import { CreateSampleCommand } from '../application/command/create_sample.command';
+import { CreateSampleCommand } from '../application/command/createSample.command';
 import { SampleResponseMessage } from '../enum/responseMessage.enum';
 
 @Controller('/sample/command')
 export class SampleCommandController {
+  /**
+   * @Providers : createSampleHandler : 샘플 작성 handler
+   */
   constructor(
     private readonly createSampleHandler: CreateSampleCommandHandler,
   ) {}
 
+  // 샘플 작성 엔드포인트 (DTO를 데이터베이스에 삽입하여 글을 작성할 수 있도록 하는 엔드포인트)
   @Post('/create')
   @HttpCode(200)
   async create(@Body() body: CreateSampleDTO) {
-    console.log(body);
     try {
-      const command = CreateSampleCommand.createCommand(body);
+      // factory method 를 통해 dto => command 로 convert
+      const command = CreateSampleCommand.create(body);
+      // command 실행.(데이터베이스에 샘플 데이터를 추가할 수 있도록)
       const result = await this.createSampleHandler.execute(command);
+      // 결과물 반환
       return result;
     } catch (error) {
+      //에러 처리
       console.error(SampleResponseMessage.__CREATE_FAILED, error);
       throw new HttpException(
         SampleResponseMessage.__CREATE_FAILED,
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
-
-    // try {
-    //   const command = new CreateSampleCommand(
-    //     nick_name,
-    //     password,
-    //     pokedex,
-    //     title,
-    //     content,
-    //     tags,
-    //   );
-    //   await this.createSampleHandler.execute(command);
-    // } catch (error) {
-
-    //   );
-    // }
   }
 }
