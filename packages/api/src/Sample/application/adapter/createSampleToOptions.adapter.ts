df513f98e60190d8e0327181fdc2e0f13fa5fd12
naviadev <qo2oq1020@gmail.com>
2024-09-02 20:18:28 +0900

    Feat | createSampleToOptions.adapter.ts : Command to Options Convert class

---
+++ packages/api/src/Sample/application/adapter/createSampleToOptions.adapter.ts
@@ -0,0 +1,23 @@
+import { CreateSampleCommand } from '../command/createSample.command';
+import CreateSampleOptions from 'src/shared/type/sampleOptions.type';
+
+export class CreateSampleCommandAdapter {
+  static toOptions(command: CreateSampleCommand): CreateSampleOptions {
+    // command => options 객체 변환
+    const data: CreateSampleOptions = {
+      pokedex: command.pokedex,
+      title: command.title,
+      ability: command.ability,
+      item: command.item,
+      content: command.content,
+      tera: command.tera,
+      evs: command.evs,
+      ivs: command.ivs,
+      id: command.id,
+      password: command.password,
+      party_tag: command.party_tag,
+      sample_tag: command.sample_tag,
+    };
+    return data;
+  }
+}
