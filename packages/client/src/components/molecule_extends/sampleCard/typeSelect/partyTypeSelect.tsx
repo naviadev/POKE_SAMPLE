ee17cbc316a4360f92cd6f371e1a15d76fbe2b4d
naviadev <qo2oq1020@gmail.com>
2024-09-03 09:59:43 +0900

    Feat | partyTypeSelect.tsx : 파티의 타입을 지정할 수 있는 select 컴포넌트 작성

---
+++ packages/client/src/components/molecule_extends/sampleCard/typeSelect/partyTypeSelect.tsx
@@ -0,0 +1,27 @@
+import partyTypeOptions from "@client/common/data/partyType";
+import SelectField from "@/components/molecule/selectField/selectField";
+import Option from "@client/common/interface/option.interface";
+interface PartyTypeProps {
+  value: Option | null;
+  onPartyTypeChange: (ability: Option | null) => void;
+}
+
+const PartyTypeSelect: React.FC<PartyTypeProps> = ({
+  value,
+  onPartyTypeChange,
+}) => {
+  return (
+    <>
+      <SelectField
+        id="party_type"
+        options={partyTypeOptions}
+        onChange={onPartyTypeChange}
+        label="파티타입"
+        placeholder="파티타입"
+        styles={{ menu: (provided: any) => ({ ...provided, zIndex: 9999 }) }}
+      />
+    </>
+  );
+};
+
+export default PartyTypeSelect;
