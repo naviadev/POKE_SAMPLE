eec69094b14ebe5329c7e3f75bf84b4440dcceaa
naviadev <qo2oq1020@gmail.com>
2024-09-03 09:57:09 +0900

    Feat | partyType.ts : select item으로 사용되는 partyType 데이터 작성

---
+++ packages/client/common/data/partyType.ts
@@ -0,0 +1,8 @@
+import Option from "../interface/option.interface"
+const partyTypeOptions: Option[] = [
+  { label: "싸이클", value: 0 },
+  { label: "대면", value: 1 },
+  { label: "랭크업", value: 2 },
+];
+
+export default partyTypeOptions;
