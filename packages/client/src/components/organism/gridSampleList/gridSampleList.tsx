fa7338f39de99a80de9ff5a5ae69576ed6364da3
naviadev <qo2oq1020@gmail.com>
2024-09-04 18:39:20 +0900

    Feat | gridSampleList.tsx : 샘플을 출력할 grid view

---
+++ packages/client/src/components/organism/gridSampleList/gridSampleList.tsx
@@ -0,0 +1,19 @@
+const GridSampleList = () => {
+  return (
+    <>
+      <h3 className="text-lg font-bold mb-4">Grid View</h3>
+      <div className="grid grid-cols-4 gap-4">
+        <div className="w-full h-24 bg-gray-300 rounded"></div>
+        <div className="w-full h-24 bg-gray-300 rounded"></div>
+        <div className="w-full h-24 bg-gray-300 rounded"></div>
+        <div className="w-full h-24 bg-gray-300 rounded"></div>
+        <div className="w-full h-24 bg-gray-300 rounded"></div>
+        <div className="w-full h-24 bg-gray-300 rounded"></div>
+        <div className="w-full h-24 bg-gray-300 rounded"></div>
+        <div className="w-full h-24 bg-gray-300 rounded"></div>
+      </div>
+    </>
+  );
+};
+
+export default GridSampleList;
