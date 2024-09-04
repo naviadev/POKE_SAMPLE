59f7ba781ec812c27457b06358a4a88f03d7c90d
naviadev <qo2oq1020@gmail.com>
2024-09-04 18:39:48 +0900

    Feat | sampleList.tsx : 목록형으로 표시될 샘플 컴포넌트

---
+++ packages/client/src/components/organism/sampleList/sampleList.tsx
@@ -0,0 +1,19 @@
+const SampleList = () => {
+  return (
+    <>
+      <div className="bg-white p-4 rounded-lg shadow-md mb-4">
+        <h3 className="text-lg font-bold mb-2">Title</h3>
+        <p className="text-sm">Author | Date</p>
+      </div>
+      <div className="bg-white p-4 rounded-lg shadow-md mb-4">
+        <h3 className="text-lg font-bold mb-2">Title</h3>
+        <p className="text-sm">Author | Date</p>
+      </div>
+      <div className="bg-white p-4 rounded-lg shadow-md mb-4">
+        <h3 className="text-lg font-bold mb-2">Title</h3>
+        <p className="text-sm">Author | Date</p>
+      </div>
+    </>
+  );
+};
+export default SampleList;
