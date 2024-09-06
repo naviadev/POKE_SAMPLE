2700e29f2424fa77105bebbda6707d6f3d0b91a2
naviadev <qo2oq1020@gmail.com>
2024-09-06 17:17:10 +0900

    Fix | page.tsx : TopNavigator 컴포넌트 테스트

---
+++ packages/client/src/app/robot/page.tsx
@@ -0,0 +1,8 @@
+import SamplePageTemplate from "@/components/template/main/mainTemplate";
+import React from "react";
+
+const HomePage = () => {
+  return <SamplePageTemplate></SamplePageTemplate>;
+};
+
+export default HomePage;
