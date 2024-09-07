26fc186e3b3d84d08582996a1720963da18e8090
naviadev <qo2oq1020@gmail.com>
2024-09-07 18:51:45 +0900

    Fix | menuButton.tsx : onClick 이벤트 삭제, useRouter 추가
    1. Props에서 onClick 삭제
    2. useRouter를 통해 MenuButton의 기능 추가

--- packages/client/src/components/molecule/menuButton/menuButton.tsx
+++ packages/client/src/components/molecule/menuButton/menuButton.tsx
@@ -1,19 +1,26 @@
 import { Button } from "@/components/atom/shad/button";
+import { useRouter } from "next/navigation";
 
 interface MenuButtonProps {
+  // 텍스트
   text: string;
-  onClick?: () => void;
+  // 버튼에 추가될 스타일
   className: string;
+  // 이동할 링크
+  href?: string;
 }
 
-const MenuButton: React.FC<MenuButtonProps> = ({
-  text,
-  onClick,
-  className,
-}) => {
+const MenuButton: React.FC<MenuButtonProps> = ({ text, className, href }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (href) {
+      router.push(href);
+    }
+  };
   return (
     <div className="w-300">
-      <Button onClick={onClick} variant="outline" className={className}>
+      <Button onClick={handleClick} variant="outline" className={className}>
         {text}
       </Button>
     </div>
