5a8da01352f669fa91365475b69f6cc1f813256e
naviadev <qo2oq1020@gmail.com>
2024-09-07 17:38:45 +0900

    Fix | menuButton.tsx : Props에 className을 추가하여 외부에서 직접 스타일링이 가능하도록 변경하였다

--- packages/client/src/components/molecule/menuButton/menuButton.tsx
+++ packages/client/src/components/molecule/menuButton/menuButton.tsx
@@ -3,12 +3,17 @@ import { Button } from "@/components/atom/shad/button";
 interface MenuButtonProps {
   text: string;
   onClick?: () => void;
+  className: string;
 }
 
-const MenuButton: React.FC<MenuButtonProps> = ({ text, onClick }) => {
+const MenuButton: React.FC<MenuButtonProps> = ({
+  text,
+  onClick,
+  className,
+}) => {
   return (
     <div className="w-300">
-      <Button onClick={onClick} variant="outline">
+      <Button onClick={onClick} variant="outline" className={className}>
         {text}
       </Button>
     </div>
