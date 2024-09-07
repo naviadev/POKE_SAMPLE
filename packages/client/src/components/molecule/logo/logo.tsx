8bff2d5546e8e3c267e6e2580eed069e32904578
naviadev <qo2oq1020@gmail.com>
2024-09-07 18:51:04 +0900

    Fix | logo.tsx : img태그 주석처리, a태그를 통해 '/' 경로로 이동할 수 있게 수정

--- packages/client/src/components/molecule/logo/logo.tsx
+++ packages/client/src/components/molecule/logo/logo.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <h1>
-      <Image src="로고 경로 추후 수정" alt="Pokesample Logo" />
+      {/* <Image src="로고 경로 추후 수정" alt="Pokesample Logo" /> */}
+      <a href="/">로고</a>
     </h1>
   );
 };
