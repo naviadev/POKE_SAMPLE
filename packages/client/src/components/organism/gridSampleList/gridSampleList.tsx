9198e4865b0374a78281afd25f8089f934b40f8b
naviadev <qo2oq1020@gmail.com>
2024-09-09 21:35:30 +0900

    Feat | gridSampleList.tsx: 샘플을 요청하고 출력할 수 있도록 기능 작성

--- packages/client/src/components/organism/gridSampleList/gridSampleList.tsx
+++ packages/client/src/components/organism/gridSampleList/gridSampleList.tsx
@@ -1,16 +1,36 @@
+"use client";
+import { useEffect, useState } from "react";
+
 const GridSampleList = () => {
+  const [data, setData] = useState<[]>();
+  const fetchData = async () => {
+    const res = await fetch("http://localhost:3001/sample/query/all");
+    const allData = await res.json();
+    setData(allData);
+    console.log(allData);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <h3 className="text-lg font-bold mb-4">Grid View</h3>
       <div className="grid grid-cols-4 gap-4">
+        {
+          // data?.map((value)=>{
+          //   return(<div key = "" className="w-full h-24 bg-gray-300 rounded">{value}</div>)
+          // })
+          }
+
+        {/* <div className="w-full h-24 bg-gray-300 rounded"></div>
         <div className="w-full h-24 bg-gray-300 rounded"></div>
         <div className="w-full h-24 bg-gray-300 rounded"></div>
         <div className="w-full h-24 bg-gray-300 rounded"></div>
         <div className="w-full h-24 bg-gray-300 rounded"></div>
         <div className="w-full h-24 bg-gray-300 rounded"></div>
-        <div className="w-full h-24 bg-gray-300 rounded"></div>
-        <div className="w-full h-24 bg-gray-300 rounded"></div>
-        <div className="w-full h-24 bg-gray-300 rounded"></div>
+        <div className="w-full h-24 bg-gray-300 rounded"></div> */}
       </div>
     </>
   );
