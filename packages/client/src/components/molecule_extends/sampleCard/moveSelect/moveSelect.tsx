b1168d0708659aa810bef82c7188f6d9d5964005
naviadev <qo2oq1020@gmail.com>
2024-09-10 18:09:37 +0900

    Fix | moveSelect.tsx : 기술 설정 컴포넌트를 동적인 생성이 가능함과 동시에, onChange 값을 할당하여 외부에서 이를 반영할 수 있도록 수정

--- packages/client/src/components/molecule_extends/sampleCard/moveSelect/moveSelect.tsx
+++ packages/client/src/components/molecule_extends/sampleCard/moveSelect/moveSelect.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import SelectField from "@/components/molecule/selectField/selectField";
 import { optionsArray } from "../../../../../common/data/move/moveOptions";
-const MoveSelect: React.FC = () => {
+import Option from "@client/common/interface/option.interface";
+
+interface MoveSelectProps {
+  moves: Option[];
+  onMoveChange: (index: number, move: Option) => void;
+}
+
+const MoveSelect: React.FC<MoveSelectProps> = ({ moves, onMoveChange }) => {
   return (
     <div className="grid grid-cols-2 gap-4">
-      <SelectField label="기술 1" options={optionsArray} />
-      <SelectField label="기술 2" options={optionsArray} />
-      <SelectField label="기술 3" options={optionsArray} />
-      <SelectField label="기술 4" options={optionsArray} />
+      {moves.map((move, index) => (
+        <SelectField
+          key={index}
+          label={`기술 ${index + 1}`}
+          placeholder="기술을 입력해주세요"
+          onChange={(newMove) => onMoveChange(index, newMove)}
+          options={optionsArray}
+        />
+      ))}
     </div>
   );
 };
