5d198864b1578eae307c47dee5ac6efc0c0cdf37
naviadev <qo2oq1020@gmail.com>
2024-09-10 15:43:46 +0900

    Fix | **getSampleHandler.ts** : @QueryHandler()의 부재로 발생했던 문제 해결
    기존 코드는 CreateSampleCommandHandler에서는 정상적으로 Repository가
    주입되지만, GetSampleHandler에는 Repository가 주입되지 않고 있었음. 이는
    Handler 데코레이터의 부재로 발생한 문제. 이를 해결하기 위해 정상적으로
    Handler 데코레이터를 추가하는 작업을 진행

--- packages/api/src/Sample/application/query/handler/getSampleHandler.ts
+++ packages/api/src/Sample/application/query/handler/getSampleHandler.ts
@@ -1,14 +1,14 @@
-import { IQueryHandler } from '@nestjs/cqrs';
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { ServiceMessage } from 'src/sample/enum/serviceMessage.enum';
 import { SampleRepository } from 'src/sample/infrastructure/repository/sample.repository';
 import { GetSampleQuery } from '../get_sample.query';
 
+@QueryHandler(GetSampleQuery)
 export class GetSampleHandler implements IQueryHandler<GetSampleQuery> {
-  constructor(private readonly sampleRepository: SampleRepository) {
-    console.log('SampleRepository:', sampleRepository); // 이 줄을 추가해 디버깅
-  }
+  constructor(private readonly sampleRepository: SampleRepository) {}
   // 모든 샘플 데이터들을 가져올 것이기 떄문에 쿼리를 매개변수로 가져오는 것을 생략.
-  async execute(): Promise<any> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async execute(query: GetSampleQuery): Promise<any> {
     const samples = await this.sampleRepository.findAll();
     if (!samples || samples.length === 0) {
       console.log('샘플을 찾을 수 없습니다.');
