520b308c85a8bd0114e4104163a71f5968180a3a
naviadev <qo2oq1020@gmail.com>
2024-09-11 22:33:06 +0900

    Feat | natureSelect.tsx : SelectField의 확장 컴포넌트 작성

---
+++ packages/client/src/components/molecule_extends/sampleCard/natureSelect/natureSelect.tsx
@@ -0,0 +1,20 @@
+import SelectField from "@/components/molecule/selectField/selectField";
+import nature from "../../../../../common/data/nature";
+import Option from "../../../../../common/interface/option.interface";
+interface NatureSelectProps {
+  onNatureChange: (nature: Option | null) => void;
+}
+const NatureSelect: React.FC<NatureSelectProps> = ({ onNatureChange }) => {
+  return (
+    <div>
+      <SelectField
+        id="nature"
+        options={nature}
+        onChange={onNatureChange}
+        label="성격"
+        placeholder="성격"
+      />
+    </div>
+  );
+};
+export default NatureSelect;
