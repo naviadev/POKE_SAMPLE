4d778a50b503530ab1abfb4cc0a148bcf12450e2
naviadev <qo2oq1020@gmail.com>
2024-09-13 10:01:31 +0900

    Fix | sample.factory.ts : nature, moves 값 추가

--- packages/api/src/Sample/domain/factory/sample.factory.ts
+++ packages/api/src/Sample/domain/factory/sample.factory.ts
@@ -14,6 +14,8 @@ import { EVs } from '../value-object/evs.vo';
 import { PartyTag } from '../value-object/partyTag.vo';
 import { SampleTag } from '../value-object/sampleTag.vo';
 import CreateSampleOptions from 'src/shared/type/sampleOptions.interface';
+import { Moves } from '../value-object/moves.vo';
+import { Nature } from '../value-object/nature.vo';
 @Injectable()
 export class SampleFactory {
   @Inject(EventPublisher) private readonly eventPublisher: EventPublisher;
@@ -33,6 +35,8 @@ export class SampleFactory {
       EVs.create(options.evs),
       PartyTag.create(options.party_tag),
       SampleTag.create(options.sample_tag),
+      Moves.create(options.moves),
+      Nature.create(options.nature),
     );
 
     // EventPublisher로 객체를 래핑하여 이벤트를 발행할 수 있도록 설정
