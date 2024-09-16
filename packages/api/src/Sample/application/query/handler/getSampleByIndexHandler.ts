a4a11e145e344e1bc3230cb12b80a5aa63697ad4
naviadev <qo2oq1020@gmail.com>
2024-09-16 16:22:37 +0900

    Feat | getSampleByIndexHandler.ts : Sample 상세 정보 출력 핸들러

---
+++ packages/api/src/Sample/application/query/handler/getSampleByIndexHandler.ts
@@ -0,0 +1,18 @@
+import { SampleRepository } from 'src/sample/infrastructure/repository/sample.repository';
+import { GetSampleByIndexQuery } from '../query/getSampleByIndex';
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+
+@QueryHandler(GetSampleByIndexQuery)
+export class GetSampleByIndexHandler implements IQueryHandler {
+  constructor(private readonly repository: SampleRepository) {}
+
+  async execute(query: GetSampleByIndexQuery) {
+    try {
+      const sampleData = await this.repository.findById(query.index);
+
+      return sampleData;
+    } catch (error) {
+      console.error(`GetSampleByIndexHandler Error : ${error}`);
+    }
+  }
+}
