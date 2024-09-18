5f5abe020d58a08e2d4c0b4310f0e8eaa4df95f7
naviadev <qo2oq1020@gmail.com>
2024-09-18 21:42:08 +0900

    Feat | sampleInfoCard.tsx : fetchData useEffect, 값을 받아와 출력.

--- packages/client/src/components/organism/sample_component/sampleInfoCard/sampleInfoCard.tsx
+++ packages/client/src/components/organism/sample_component/sampleInfoCard/sampleInfoCard.tsx
@@ -5,11 +5,37 @@ import SampleInfo from "@/components/molecule/sample/sampleInfo/sampleInfo";
 import SampleStats from "@/components/molecule/sample/sampleStats/sampleStats";
 import Skill from "@/components/molecule/sample/skillBox/skillBox";
 import SampleDesForm from "@/components/molecule_extends/sampleDescription/SampleDesForm";
+import Sample from "@client/common/interface/sample.interface";
 
 import Image from "next/image";
+import React, { useCallback, useEffect, useState } from "react";
 
+interface SampleInfoComponentProps {
+  index: number;
+}
+/**
+ * @pseudocode
+ * details 값을 props로 받으며, 해당 값이 변경될 때, useEffect 값 .
+ * onClick으로 fetch ->
+ * Grid Item Onclick =>
+ *
+ *
+ */
+const SampleInfoComponent: React.FC<SampleInfoComponentProps> = ({ index }) => {
+  const [data, setData] = useState<Sample>();
 
-const SampleInfoComponent = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `http://localhost:3001/sample/query/sampleWithIndex/${index}`
+      );
+      const result = await res.json();
+      setData(result);
+    };
+    fetchData();
+  }, [index]);
+  console.log(data);
+
   return (
     <Card className="w-80 p-6 bg-white rounded-xl shadow-md mx-auto">
       {/* 상단 영역 - 이미지 수정버튼*/}
@@ -31,8 +57,8 @@ const SampleInfoComponent = () => {
         </div>
         <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-16 h-16 bg-gray-300 rounded-full border-4 border-white">
           <Image
-            src="/sprite_pokemon_images_webp/233.webp"
-            alt="폴리곤 이미지"
+            src={`/sprite_pokemon_images_webp/${data?.pokedex}.webp`}
+            alt={data?.pokemon!}
             width={64}
             height={64}
             className="rounded-full"
@@ -42,18 +68,18 @@ const SampleInfoComponent = () => {
 
       {/* 프로필 정보 */}
       <SampleInfo
-        sampleName="물리막이  폴리곤"
-        partyType="싸이클"
-        sampleType="물리막이"
+        sampleName={data?.title!}
+        partyType={data?.party_tag!}
+        sampleType={data?.sample_tag!}
       ></SampleInfo>
 
       <SampleStats recommendCount="200" viewCount="200"></SampleStats>
 
       {/* 아이템, 지닌물건, 특성, 성격 텍스트 */}
       <div className="text-gray-600 mt-4 px-4 space-y-2 ">
-        <SampleDesForm labelText="성격" pText="괘씸" />
-        <SampleDesForm labelText="도구" pText="진화의휘석" />
-        <SampleDesForm labelText="특성" pText="다운로드" />
+        <SampleDesForm labelText="성격" pText={data?.nature!} />
+        <SampleDesForm labelText="도구" pText={data?.item!} />
+        <SampleDesForm labelText="특성" pText={data?.ability!} />
       </div>
 
       {/* 기술 배치 */}
@@ -62,10 +88,10 @@ const SampleInfoComponent = () => {
       </div>
 
       <div className="grid grid-cols-2 gap-4 justify-items-center mt-4">
-        <Skill name="방전"></Skill>
-        <Skill name="전기자석파"></Skill>
-        <Skill name="HP회복"></Skill>
-        <Skill name="트라이어택"></Skill>
+        <Skill name={data?.moves[0]!}></Skill>
+        <Skill name={data?.moves[1]!}></Skill>
+        <Skill name={data?.moves[2]!}></Skill>
+        <Skill name={data?.moves[3]!}></Skill>
       </div>
 
       {/* 하단 버튼 */}
@@ -81,4 +107,4 @@ const SampleInfoComponent = () => {
   );
 };
 
-export default SampleInfoComponent;
+export default React.memo(SampleInfoComponent);
