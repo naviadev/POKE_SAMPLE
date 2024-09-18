43400070c1cf32f2e497e2ef9d7d77f2e116929c
naviadev <qo2oq1020@gmail.com>
2024-09-18 19:58:00 +0900

    Fix | gridSampleList.tsx : Item을 사용하는 방식으로 수정
    기존 방식은 GridSampleList에서 Item에 대한 컴포넌트를 직접 작성하여
    반복하는 방식이였지만, Item 컴포넌트를 분리함으로써 직관성 있는 형태의
    컴포넌트로 수정하였다.

--- packages/client/src/components/organism/sample_component/gridSampleList/gridSampleList.tsx
+++ packages/client/src/components/organism/sample_component/gridSampleList/gridSampleList.tsx
@@ -1,12 +1,6 @@
-import Image from "next/image";
 import { useEffect, useState } from "react";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@client/src/components/atom/shad/card";
-import { Section } from "lucide-react";
+import SampleItem from "@/components/molecule_extends/sampleItem/sampleItem";
+import { SampleData } from "@/components/molecule_extends/sampleItem/interface/sampleItem.props";
 
 const GridSampleList = () => {
   const [data, setData] = useState([]);
@@ -15,6 +9,7 @@ const GridSampleList = () => {
     const res = await fetch("http://localhost:3001/sample/query/latest");
     const allData = await res.json();
     setData(allData);
+    console.log(allData);
   };
 
   useEffect(() => {
@@ -27,42 +22,8 @@ const GridSampleList = () => {
       <h2 className="text-3xl font-bold mb-6 text-gray-800">샘플</h2>
       {/* Grid Div */}
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-        {data?.map((value: { [key: string]: any }, index) => (
-          <Card
-            key={index}
-            className="relative overflow-hidden hover:shadow-2xl transition-shadow duration-300 group"
-          >
-            <CardHeader className="p-4 bg-gray-50 relative">
-              {/* 애니메이션이 있는 SampleTag */}
-              <div className="absolute top-2 left-2 overflow-hidden">
-                <div className="bg-gray-200 text-black rounded-full w-6 h-6 flex items-center justify-center transition-all duration-300 ease-in-out group-hover:w-auto group-hover:px-3 group-hover:py-1">
-                  <span className="text-xs font-semibold whitespace-nowrap overflow-hidden transition-opacity duration-300 opacity-0 group-hover:opacity-100">
-                    {value.sampleTag.value}
-                  </span>
-                </div>
-              </div>
-
-              <div className="w-24 h-24 mx-auto rounded-full overflow-hidden bg-white shadow-inner">
-                <Image
-                  src={`/sprite_pokemon_images_webp/${value.pokedex.value}.webp`}
-                  alt="Pokemon Image"
-                  width={96}
-                  height={96}
-                  className="object-contain"
-                />
-              </div>
-            </CardHeader>
-            <CardContent className="p-4">
-              <CardTitle className="text-lg font-semibold mb-2 text-center text-gray-800">
-                {value.title.value}
-              </CardTitle>
-              <div className="flex justify-center space-x-2 text-sm text-gray-600">
-                <span className="bg-blue-100 text-blue-800 px-2 py-1 rounded-full">
-                  {value.item.value}
-                </span>
-              </div>
-            </CardContent>
-          </Card>
+        {data?.map((value: SampleData, index) => (
+          <SampleItem key={index} index={value.index} sampleData={value} />
         ))}
       </section>
     </div>
