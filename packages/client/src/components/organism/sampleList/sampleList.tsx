b56e1bb955797ed9eb8a4451de529ae975e3a595
naviadev <qo2oq1020@gmail.com>
2024-09-18 21:45:26 +0900

    Fix | sampleList.tsx : React.memo를 통해 export 래핑

--- packages/client/src/components/organism/sampleList/sampleList.tsx
+++ packages/client/src/components/organism/sampleList/sampleList.tsx
@@ -1,4 +1,7 @@
+import React from "react";
+
 const SampleList = () => {
+  console.log("상관없는 컴포넌트 리렌더");
   return (
     <>
       <div className="bg-white p-4 rounded-lg shadow-md mb-4">
@@ -16,4 +19,4 @@ const SampleList = () => {
     </>
   );
 };
-export default SampleList;
+export default React.memo(SampleList);
