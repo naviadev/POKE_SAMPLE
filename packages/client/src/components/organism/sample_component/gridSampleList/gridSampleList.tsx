97c29f97f24b5aa38c505d9c4907034a7c1956ee
naviadev <qo2oq1020@gmail.com>
2024-09-19 17:29:21 +0900

    Feat | gridSample.tsx : page 전환 컴포넌트 추가

--- packages/client/src/components/organism/sample_component/gridSampleList/gridSampleList.tsx
+++ packages/client/src/components/organism/sample_component/gridSampleList/gridSampleList.tsx
@@ -1,6 +1,7 @@
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import SampleItem from "@/components/molecule_extends/sampleItem/sampleItem";
 import { SampleData } from "@/components/molecule_extends/sampleItem/interface/sampleItem.props";
+import MoleculePagination from "@/components/molecule_extends/pagination/pagination";
 
 interface GridSampleListProps {
   setInfoIndex: (newIndex: number) => void;
@@ -22,7 +23,9 @@ const GridSampleList: React.FC<GridSampleListProps> = ({ setInfoIndex }) => {
   // 그리드를 통해 샘플을 출력하는 컴포넌트
   return (
     <div className="container mx-auto px-4 py-8">
-      <h2 className="text-3xl font-bold mb-6 text-gray-800">샘플</h2>
+      <div className="sticky top-0 z-10 bg-white">
+        <h2 className="text-3xl font-bold mb-6 text-gray-800">샘플</h2>
+      </div>
       {/* Grid Div */}
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
         {data?.map((value: SampleData, index) => (
@@ -35,6 +38,7 @@ const GridSampleList: React.FC<GridSampleListProps> = ({ setInfoIndex }) => {
             }}
           />
         ))}
+        <MoleculePagination />
       </section>
     </div>
   );
