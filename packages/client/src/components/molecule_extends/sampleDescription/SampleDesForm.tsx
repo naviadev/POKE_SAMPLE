7d1b51e14dd52fab77c305b740f07bd13c3472ed
naviadev <qo2oq1020@gmail.com>
2024-09-19 17:28:43 +0900

    Feat | sampleDesForm.tsx : 샘플 성격, 도구, 특성을 Hover로 출력

--- packages/client/src/components/molecule_extends/sampleDescription/SampleDesForm.tsx
+++ packages/client/src/components/molecule_extends/sampleDescription/SampleDesForm.tsx
@@ -1,17 +1,37 @@
 import { Label } from "@/components/atom/shad/label";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/atom/shad/tooltip";
 
 interface SampleDesFormProps {
   labelText: string;
   pText: string;
 }
-const SampleDesForm: React.FC<SampleDesFormProps> = ({ labelText, pText }) => {
+// const SampleDesForm: React.FC<SampleDesFormProps> = ({ labelText, pText }) => {
+//   return (
+//     <div className="flex flex-col items-center space-y-2 ">
+//       <Label htmlFor="" className="text-gray-500">
+//         {labelText}
+//       </Label>
+//       <p className="font-medium">{pText}</p>
+//     </div>
+//   );
+// };
+
+const SampleDesForm : React.FC<SampleDesFormProps> = ({labelText, pText}) => {
   return (
-    <div className="flex flex-col items-center space-y-2 ">
-      <Label htmlFor="" className="text-gray-500">
-        {labelText}
-      </Label>
-      <p className="font-medium">{pText}</p>
-    </div>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>{pText}</TooltipTrigger>
+        <TooltipContent>
+          <p>{labelText}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
+
 export default SampleDesForm;
