90eef9da3c1d5d24e8e27eb9fc8bbea6bae54d00
naviadev <qo2oq1020@gmail.com>
2024-09-19 17:29:42 +0900

    Feat | mainTemplate.tsx : 네비게이터 버튼 추가

--- packages/client/src/components/template/main/mainTemplate.tsx
+++ packages/client/src/components/template/main/mainTemplate.tsx
@@ -1,10 +1,10 @@
+import NavigatorButton from "@/components/molecule/navigatorButton/navigatorButton";
 import GridSampleList from "@/components/organism/sample_component/gridSampleList/gridSampleList";
 import SampleInfoCard from "@/components/organism/sample_component/sampleInfoCard/sampleInfoCard";
 import SampleList from "@/components/organism/sampleList/sampleList";
 import { useCallback, useState } from "react";
 
 const SamplePageTemplate = () => {
-  // useMemo를 통해 리렌더링 방지.
   const [details, setDetails] = useState<number>(1);
 
   const handleSetDetails = useCallback((newIndex: number) => {
@@ -12,23 +12,28 @@ const SamplePageTemplate = () => {
   }, []);
 
   return (
-    <>
-      <div className="min-h-screen bg-gray-100 w-screen flex justify-center items-center pt-7">
-        {/* 메인 컨텐츠 */}
-        <main className="flex p-4 w-9/12 gap-6">
-          {/* 왼쪽 카드 섹션 */}
-          <SampleInfoCard index={details} />
-          {/* <SampleInfoCard></SampleInfoCard> */}
-          {/* 오른쪽 컨텐츠 섹션 */}
-          <section className="w-3/4 p-4">
+    <div className="min-h-screen bg-gray-100 w-full flex justify-center items-center">
+      <div className="w-9/12 flex justify-between">
+        {/* 왼쪽 고정 섹션 */}
+        <aside className="fixed left-1/2 transform -translate-x-[600px]">
+          <div className="py-6">
+            <SampleInfoCard index={details} />
+          </div>
+        </aside>
+        {/* 오른쪽 스크롤 가능한 섹션 */}
+        <main className="w-3/4 ml-auto py-7">
+          <section className="p-4">
             {/* 목록 섹션 */}
-            <SampleList></SampleList>
+            <SampleList />
             {/* 그리드 뷰 섹션 */}
-            <GridSampleList setInfoIndex={setDetails}></GridSampleList>
+            <GridSampleList setInfoIndex={handleSetDetails} />
           </section>
         </main>
+
+        <NavigatorButton />
       </div>
-    </>
+    </div>
   );
 };
+
 export default SamplePageTemplate;
