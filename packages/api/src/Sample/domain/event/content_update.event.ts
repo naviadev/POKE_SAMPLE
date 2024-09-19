c11ba970dc67584a4e02045c688d51978127668f
naviadev <qo2oq1020@gmail.com>
2024-09-19 17:27:16 +0900

    Fix | content_update.event.ts : Index 삭제로 임시처리

--- packages/api/src/Sample/domain/event/content_update.event.ts
+++ packages/api/src/Sample/domain/event/content_update.event.ts
@@ -1,16 +1,13 @@
-import { Index } from '../value-object/index.vo';
 import { Title } from '../value-object/title.vo';
 import { Content } from '../value-object/content.vo';
-import { Tag } from '../value-object/tag.vo';
+
 import { IEvent } from '@nestjs/cqrs';
 /**
  * 추상적인 형태로 이벤트를 정의.
  */
 export class SampleUpdatedEvent implements IEvent {
   constructor(
-    public readonly id: Index,
     public readonly newTitle: Title,
     public readonly newContent: Content,
-    public readonly newTags: Tag,
   ) {}
 }
