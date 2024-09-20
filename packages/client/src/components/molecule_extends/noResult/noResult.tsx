28d8ee625b8a3d3622007893f917cf4ff223baf8
naviadev <qo2oq1020@gmail.com>
2024-09-20 17:33:19 +0900

    Feat | noResult.tsx : 결과가 출력되지 않았을 경우 사용되는 컴포넌트

---
+++ packages/client/src/components/molecule_extends/noResult/noResult.tsx
@@ -0,0 +1,10 @@
+import DivInH2 from "@/components/molecule/divInH2/divInH2";
+
+const NoResult = () => {
+  return (
+    <DivInH2 divClassName="my-div-class" h2ClassName="my-h2-class">
+      검색 결과가 없습니다.
+    </DivInH2>
+  );
+};
+export default NoResult;
