6dde9d892e20eedcc5cdb20be71ce09633c4f7d3
naviadev <qo2oq1020@gmail.com>
2024-09-20 10:44:10 +0900

    Feat | sample.query.controller.ts : 검색 엔드포인트 작성

--- packages/api/src/Sample/presentation/controller/sample.query.controller.ts
+++ packages/api/src/Sample/presentation/controller/sample.query.controller.ts
@@ -10,8 +10,10 @@ import { SampleResponseMessage } from '../../enum/message/responseMessage.enum';
 import { GetSampleQuery } from '../../application/query/query/getSample.query';
 import { GetLatestSampleQuery } from '../../application/query/query/getLatestSample.query';
 import { GetLatestSampleHandler } from '../../application/query/handler/getLatestSampleHandler';
-import { GetSampleByIndexQuery } from 'src/sample/application/query/query/getSampleByIndex';
+import { GetSampleByIndexQuery } from 'src/sample/application/query/query/getSampleByIndex.query';
 import { GetSampleByIndexHandler } from 'src/sample/application/query/handler/getSampleByIndexHandler';
+import { GetSampleByPokedex } from 'src/sample/application/query/query/getSampleByPokedex.query';
+import { GetSampleByPokedexHandler } from 'src/sample/application/query/handler/getSampleByPokedexHandler';
 
 @Controller('/sample/query')
 export class SampleQueryController {
@@ -19,6 +21,7 @@ export class SampleQueryController {
     private readonly sampleQueryHandler: GetSampleHandler,
     private readonly sampleLatestQueryHandler: GetLatestSampleHandler,
     private readonly sampleByIndexHandler: GetSampleByIndexHandler,
+    private readonly sampleByPokedexHandler: GetSampleByPokedexHandler,
   ) {}
 
   /**
@@ -41,6 +44,10 @@ export class SampleQueryController {
     }
   }
 
+  /**
+   * @description 최신의 게시물 20개를 정렬하여 가져오는 엔드포인트.
+   * @returns 최신 게시글 20개의 기본데이터 (타이틀, 포켓몬, 도구, 샘플 타입)
+   */
   @Get('/latest')
   async latestSample() {
     const result = await this.sampleLatestQueryHandler.execute(
@@ -49,10 +56,25 @@ export class SampleQueryController {
     return result;
   }
 
+  /**
+   *
+   * @param index 게시글의 고유 PK
+   * @returns PK를 통해 해당 게시글의 상세 내용을 가져오는 것을 목적으로 하는 엔드포인트.
+   */
   @Get('/sampleWithIndex/:index')
   async sampleWithIndex(@Param('index') index: number) {
     const query = await GetSampleByIndexQuery.create(index);
     const data = await this.sampleByIndexHandler.execute(query);
+    return data;
+  }
+
+  @Get('/sampleWithPokedex/:pokedex/:number')
+  async sampleByPokedex(
+    @Param('pokedex') pokedex: number,
+    @Param('number') number: number,
+  ) {
+    const query = await GetSampleByPokedex.create(pokedex, number);
+    const data = await this.sampleByPokedexHandler.execute(query);
     return data;
   }
 }
