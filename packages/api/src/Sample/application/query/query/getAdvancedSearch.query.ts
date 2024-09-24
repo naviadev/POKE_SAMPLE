28f395e6451e91c61bc6767aa6e3a1b4783e91cf
naviadev <qo2oq1020@gmail.com>
2024-09-24 19:41:57 +0900

    Feat | GetAdvancedSearch.query.ts : 상세 검색 쿼리 작성

---
+++ packages/api/src/Sample/application/query/query/getAdvancedSearch.query.ts
@@ -0,0 +1,29 @@
+import { IQuery } from '@nestjs/cqrs';
+
+export class GetAdvancedSearch implements IQuery {
+  constructor(
+    public readonly pokemon: string,
+    public readonly sample_tag?: string,
+    public readonly party_tag?: string,
+  ) {}
+
+  static create(queryString: string) {
+    return this.convertQuery(queryString);
+  }
+
+  private static convertQuery(queryString: string) {
+    const queryArray = queryString['0'].split('/'); // ex ) "pokemon/25/sample_tag/tag_value"
+
+    const query: { [key: string]: string } = {};
+
+    for (let i = 0; i < queryArray.length; i += 2) {
+      query[queryArray[i]] = queryArray[i + 1];
+    }
+
+    return new GetAdvancedSearch(
+      query['pokemon'],
+      query['sample_tag'],
+      query['party_tag'],
+    );
+  }
+}
