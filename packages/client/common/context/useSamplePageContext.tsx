bcb5e298d36fa02f95fcf125f60381a5cef0cf69
naviadev <qo2oq1020@gmail.com>
2024-09-24 19:39:38 +0900

    Feat | useSamplePageContext.ts : useContext 훅을 사용하여 상태를 관리.
    1. 기존 코드는 Props를 통해 자식 노드로 값을 전달하는 방식이였지만,
       Atomic 패턴 적용 과정에서 불편함이 발생할 수 있기에, Context와
       Provider, Reducer 를 통해 객체형 상태 변수를 Provider 내부에서 사용할
       수 있도록 작성하였다.

---
+++ packages/client/common/context/useSamplePageContext.tsx
@@ -0,0 +1,84 @@
+import Sample from "@client/common/interface/sample.interface";
+import React, { createContext, ReactNode, useContext, useReducer } from "react";
+
+interface SamplePageState {
+  index: number;
+  filteredData: Sample[];
+  saveLatestData: Sample[];
+}
+
+type Action =
+  | { type: "SET_DETAILS"; payload: number }
+  | { type: "SET_INDEX"; payload: number }
+  | { type: "SET_FILTERED_DATA"; payload: Sample[] }
+  | { type: "SET_SAVE_LATEST_DATE"; payload: Sample[] };
+
+const SamplePageContext = createContext<
+  { state: SamplePageState; dispatch: React.Dispatch<Action> } | undefined
+>(undefined);
+
+const SamplePageReducer = (
+  state: SamplePageState,
+  action: Action
+): SamplePageState => {
+  switch (action.type) {
+    case "SET_FILTERED_DATA":
+      return { ...state, filteredData: action.payload };
+    case "SET_SAVE_LATEST_DATE":
+      return { ...state, saveLatestData: action.payload };
+    case "SET_INDEX":
+      return { ...state, index: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const SamplePageProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [state, dispatch] = useReducer(SamplePageReducer, {
+    index: 1,
+    filteredData: [],
+    saveLatestData: [],
+  });
+
+  return (
+    <SamplePageContext.Provider value={{ state, dispatch }}>
+      {children}
+    </SamplePageContext.Provider>
+  );
+};
+
+/**
+ * @returns {handleChangeIndex, handleChangeFilteredData, state, index, filteredData}
+ * @state 모든 상태변수를 객체의 형태로 반환.
+ * @index 상세 정보 출력을 Index 값
+ * @filteredData 화면에 출력되는 값
+ * @handleChange * 상태변수 setter
+ */
+export const useContextSamplePage = () => {
+  const context = useContext(SamplePageContext);
+
+  if (context === undefined) {
+    throw new Error("에러");
+  }
+  const { state, dispatch } = context;
+
+  const index = state.index;
+  const filteredData = state.filteredData;
+
+  const handleChangeIndex = (value: number) => {
+    dispatch({ type: "SET_INDEX", payload: value });
+  };
+
+  const handleChangeFilteredData = (value: Sample[]) => {
+    dispatch({ type: "SET_FILTERED_DATA", payload: value });
+  };
+  return {
+    handleChangeIndex,
+    handleChangeFilteredData,
+    state,
+    index,
+    filteredData,
+  };
+};
