ca0451c9dfa229b9429bdd7bac8e67f8b5cbb023
naviadev <qo2oq1020@gmail.com>
2024-09-24 19:38:37 +0900

    Style | apiUrl.enum.ts : ADVANCED_SEARCH, MORE_SAMPLES url 추가

--- packages/client/common/enum/apiUrl.enum.ts
+++ packages/client/common/enum/apiUrl.enum.ts
@@ -1,5 +1,8 @@
 export enum API_URL {
   SEARCH_URL = `http://localhost:3001/sample/query/sampleWithPokedex`,
   GET_LATEST_SAMPLE = "http://localhost:3001/sample/query/latest",
-  GET_MORE_SAMPLES = "http://localhost:3001/sample/query/sampleByIndexScroll"
+  GET_MORE_SAMPLES = "http://localhost:3001/sample/query/sampleByIndexScroll",
+  ADVANCED_SEARCH = "http://localhost:3001/sample/query/advanced"
+
+
 }
