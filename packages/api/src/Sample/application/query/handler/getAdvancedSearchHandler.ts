db56e6b2907381f1a482eb8a38cfe56beed8c268
naviadev <qo2oq1020@gmail.com>
2024-09-26 17:48:45 +0900

    Feat | getAdvancedSearchHandler.ts : QueryHandler 작성

---
+++ packages/api/src/Sample/application/query/handler/getAdvancedSearchHandler.ts
@@ -0,0 +1,30 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { GetAdvancedSearch } from '../query/getAdvancedSearch.query';
+import { SampleRepository } from 'src/sample/infrastructure/repository/sample.repository';
+
+@QueryHandler(GetAdvancedSearch)
+export class GetAdvancedSearchHandler
+  implements IQueryHandler<GetAdvancedSearch>
+{
+  constructor(private readonly sampleRepository: SampleRepository) {}
+
+  async execute(query: GetAdvancedSearch): Promise<any> {
+    const filteredData = {};
+    //Query 객체를 순회하며, undefined를 제외하여 사용자가 요청한 데이터만을 추린다.
+    for (const key in query) {
+      if (query[key] !== undefined) {
+        filteredData[key] = query[key];
+      }
+    }
+    console.log(filteredData);
+    try {
+      const data =
+        await this.sampleRepository.findAdvancedSearchData(filteredData);
+      return data;
+    } catch (error) {
+      // 에러 로깅 및 처리를 여기에서 수행
+      console.error('Error creating sample:', error);
+      throw new Error('Sample creation failed');
+    }
+  }
+}
