a5232c863e218731089afc4d6a689f6efe8dd695
naviadev <qo2oq1020@gmail.com>
2024-10-04 19:15:06 +0900

    Feat | pokemonImageType.tsx : 포켓몬 이미지, 타입을 래핑한 컴포넌트

---
+++ packages/client/src/components/molecule_extends/pokemonImageType/pokemonImageType.tsx
@@ -0,0 +1,43 @@
+import TypeBar from "@/components/molecule/typeBar/typeBar";
+import Option from "@client/common/interface/option.interface";
+import Image from "next/image";
+
+interface PokemonImageTypeProps {
+  pokedex: number | string | null;
+  type: Option[] | Option | null;
+}
+
+const PokemonImageType: React.FC<PokemonImageTypeProps> = ({
+  pokedex,
+  type,
+}) => {
+  return (
+    <div className="flex flex-col justify-center gap-12 sm:flex-row items-center sm:items-start w-full">
+      {/* 이미지 , 타입 컴포넌트 */}
+      <div className="flex flex-col justify-center items-center space-y-6 mb-6 sm:mb-0">
+        <div className="w-24 h-24 rounded-full overflow-hidden bg-white shadow-inner">
+          {pokedex ? (
+            <Image
+              src={`/sprite_pokemon_images_webp/${pokedex}.webp`}
+              alt="Pokemon Image"
+              width={96}
+              height={96}
+              className="object-contain"
+            />
+          ) : null}
+        </div>
+        <div className="flex justify-center space-x-4">
+          {Array.isArray(type) ? (
+            type.map((value, index) => {
+              return <TypeBar type={value} key={index} />;
+            })
+          ) : (
+            <TypeBar type={type} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonImageType;
