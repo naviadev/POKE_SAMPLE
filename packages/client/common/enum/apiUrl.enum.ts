8add200ac9a575f57d2f8037d06367fe4300f9cb
naviadev <qo2oq1020@gmail.com>
2024-10-12 18:30:34 +0900

    Feat | apiUrl.enum.ts : type 검색을 위한 엔드포인트 추가

--- packages/client/common/enum/apiUrl.enum.ts
+++ packages/client/common/enum/apiUrl.enum.ts
@@ -2,7 +2,6 @@ export enum API_URL {
   SEARCH_URL = `http://localhost:3001/sample/query/sampleWithPokedex`,
   GET_LATEST_SAMPLE = "http://localhost:3001/sample/query/latest",
   GET_MORE_SAMPLES = "http://localhost:3001/sample/query/sampleByIndexScroll",
-  ADVANCED_SEARCH = "http://localhost:3001/sample/query/advanced"
-
-
+  ADVANCED_SEARCH = "http://localhost:3001/sample/query/advanced",
+  TYPE_SEARCH = "http://localhost:3001/pokemon/query/types"
 }
