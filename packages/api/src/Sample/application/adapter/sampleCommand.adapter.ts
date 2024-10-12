630ff3743e58fcf92563b76918baff3063151f4a
naviadev <qo2oq1020@gmail.com>
2024-10-12 18:29:59 +0900

    Fix | Sample Command : 임시 주석처리

--- packages/api/src/Sample/application/adapter/sampleCommand.adapter.ts
+++ packages/api/src/Sample/application/adapter/sampleCommand.adapter.ts
@@ -1,25 +1,25 @@
-import { CreateSampleCommand } from '../command/command/createSample.command';
-import CreateSampleOptions from 'src/shared/type/sampleOptions.interface';
+// import { CreateSampleCommand } from '../command/command/createSample.command';
+// import CreateSampleOptions from 'src/shared/type/sampleOptions.interface';
 
-export class CreateSampleCommandAdapter {
-  static toOptions(command: CreateSampleCommand): CreateSampleOptions {
-    // command => options 객체 변환
-    const data: CreateSampleOptions = {
-      pokedex: command.pokedex,
-      title: command.title,
-      ability: command.ability,
-      item: command.item,
-      content: command.content,
-      tera: command.tera,
-      evs: command.evs,
-      ivs: command.ivs,
-      id: command.id,
-      password: command.password,
-      party_tag: command.party_tag,
-      sample_tag: command.sample_tag,
-      moves: command.moves,
-      nature: command.nature,
-    };
-    return data;
-  }
-}
+// export class CreateSampleCommandAdapter {
+//   static toOptions(command: CreateSampleCommand): CreateSampleOptions {
+//     // command => options 객체 변환
+//     const data: CreateSampleOptions = {
+//       pokedex: command.pokedex,
+//       title: command.title,
+//       ability: command.ability,
+//       item: command.item,
+//       content: command.content,
+//       tera: command.tera,
+//       evs: command.evs,
+//       ivs: command.ivs,
+//       id: command.id,
+//       password: command.password,
+//       party_tag: command.party_tag,
+//       sample_tag: command.sample_tag,
+//       moves: command.moves,
+//       nature: command.nature,
+//     };
+//     return data;
+//   }
+// }
--- packages/api/src/Sample/application/command/command/createSample.command.ts
+++ packages/api/src/Sample/application/command/command/createSample.command.ts
@@ -1,40 +1,12 @@
-import { ICommand } from '@nestjs/cqrs';
-import { CreateSampleDTO } from 'src/sample/presentation/dto/createSample.dto';
-
-// CreateSampleCommand : 샘플 게시글 작성 커맨드
-export class CreateSampleCommand implements ICommand {
-  constructor(
-    public readonly pokedex: number,
-    public readonly title: string,
-    public readonly ability: string,
-    public readonly item: string,
-    public readonly content: string,
-    public readonly tera: string,
-    public readonly evs: string,
-    public readonly ivs: string,
-    public readonly id: string,
-    public readonly password: string,
-    public readonly party_tag: string,
-    public readonly sample_tag: string,
-    public readonly moves: string[],
-    public readonly nature: string,
-  ) {}
-  static create(dto: CreateSampleDTO) {
-    return new CreateSampleCommand(
-      dto.pokedex,
-      dto.title,
-      dto.ability,
-      dto.item,
-      dto.content,
-      dto.tera,
-      dto.evs,
-      dto.ivs,
-      dto.id,
-      dto.password,
-      dto.party_tag,
-      dto.sample_tag,
-      dto.moves,
-      dto.nature,
-    );
-  }
-}
+// export class CreateSampleCommand {
+//   constructor(
+//     public readonly pokedex: number,
+//     public readonly abilities: string,
+//     public readonly title: string,
+//     public readonly content: string,
+//     public readonly sampleTagId: number,
+//     public readonly itemId: number,
+//     public readonly natureId: number,
+//     public readonly tera: number,
+//   ) {}
+// }
