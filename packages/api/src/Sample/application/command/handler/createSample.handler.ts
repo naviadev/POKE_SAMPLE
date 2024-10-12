c3f29f67a73ca49b79b2d6f359450b307b47f226
naviadev <qo2oq1020@gmail.com>
2024-10-12 17:39:38 +0900

    Fix | sample/*.ts : 일부 코드 로직 주석처리.
    변경된 Entity에 맞춰 도메인 로직을 재구성하기 위해 일부 로직을
    임시적으로 제외

--- packages/api/src/Sample/application/command/handler/createSample.handler.ts
+++ packages/api/src/Sample/application/command/handler/createSample.handler.ts
@@ -1,45 +1,45 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { CreateSampleCommand } from '../command/createSample.command';
-import { SampleFactory } from 'src/sample/domain/factory/sample.factory';
-import { SampleRepository } from 'src/sample/infrastructure/repository/sample.repository';
-import { DataSource, QueryRunner } from 'typeorm';
-import { ServiceMessage } from 'src/sample/enum/message/serviceMessage.enum';
-import { InjectDataSource } from '@nestjs/typeorm';
-import { CreateSampleCommandAdapter } from '../../adapter/sampleCommand.adapter';
+// import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+// import { CreateSampleCommand } from '../command/createSample.command';
+// import { SampleFactory } from 'src/sample/domain/factory/sample.factory';
+// import { SampleRepository } from 'src/sample/infrastructure/repository/sample.repository';
+// import { DataSource, QueryRunner } from 'typeorm';
+// import { ServiceMessage } from 'src/sample/enum/message/serviceMessage.enum';
+// import { InjectDataSource } from '@nestjs/typeorm';
+// import { CreateSampleCommandAdapter } from '../../adapter/sampleCommand.adapter';
 
-/**
- * * Class : CreateSampleCommandHandler
- * 작성자 : @naviadev / 2024-08-17
- * @class CreateSampleCommandHandler
- * @param private readonly sampleFactory: SampleFactory, private readonly sampleRepository: SampleRepository
- * @description : Sample 상태 변경 요청을 처리하며, 비즈니스 로직을 실행하는 역할을 수행한다.
- */
-@CommandHandler(CreateSampleCommand)
-export class CreateSampleCommandHandler
-  implements ICommandHandler<CreateSampleCommand>
-{
-  private readonly queryRunner: QueryRunner;
-  constructor(
-    private readonly sampleFactory: SampleFactory, // 도메인 생성을 위한 팩토리 .
-    private readonly sampleRepository: SampleRepository, // 도메인 저장을 위한 레포지토리 .
-    @InjectDataSource() private readonly dataSource: DataSource, // 트랜잭션 관리 모듈 주입.
-  ) {}
-  async execute(command: CreateSampleCommand): Promise<any> {
-    const queryRunner = this.dataSource.createQueryRunner(); // QueryRunner 생성
-    const options = CreateSampleCommandAdapter.toOptions(command);
+// /**
+//  * * Class : CreateSampleCommandHandler
+//  * 작성자 : @naviadev / 2024-08-17
+//  * @class CreateSampleCommandHandler
+//  * @param private readonly sampleFactory: SampleFactory, private readonly sampleRepository: SampleRepository
+//  * @description : Sample 상태 변경 요청을 처리하며, 비즈니스 로직을 실행하는 역할을 수행한다.
+//  */
+// @CommandHandler(CreateSampleCommand)
+// export class CreateSampleCommandHandler
+//   implements ICommandHandler<CreateSampleCommand>
+// {
+//   private readonly queryRunner: QueryRunner;
+//   constructor(
+//     private readonly sampleFactory: SampleFactory, // 도메인 생성을 위한 팩토리 .
+//     private readonly sampleRepository: SampleRepository, // 도메인 저장을 위한 레포지토리 .
+//     @InjectDataSource() private readonly dataSource: DataSource, // 트랜잭션 관리 모듈 주입.
+//   ) {}
+//   async execute(command: CreateSampleCommand): Promise<any> {
+//     const queryRunner = this.dataSource.createQueryRunner(); // QueryRunner 생성
+//     const options = CreateSampleCommandAdapter.toOptions(command);
 
-    await queryRunner.connect();
-    await queryRunner.startTransaction(); // 트랜잭션 작성 시작. (하나의 처리로 묶는 작업) // ? 확장성을 고려한 작업.
+//     await queryRunner.connect();
+//     await queryRunner.startTransaction(); // 트랜잭션 작성 시작. (하나의 처리로 묶는 작업) // ? 확장성을 고려한 작업.
 
-    try {
-      const sample = this.sampleFactory.create(options); // Factory 객체를 통해 Sample 도메인 인스턴스.
-      await this.sampleRepository.save(sample); // 커스텀 Repository를 통해 Database 쓰기 작업 수행
-      await queryRunner.commitTransaction(); // 트랜잭션 커밋.
-    } catch (error) {
-      console.error(ServiceMessage.__COMMAND_EXEC_FAILED, error);
-      throw error;
-    } finally {
-      await queryRunner.release(); // 트랜잭션 해제
-    }
-  }
-}
+//     try {
+//       const sample = this.sampleFactory.create(options); // Factory 객체를 통해 Sample 도메인 인스턴스.
+//       await this.sampleRepository.save(sample); // 커스텀 Repository를 통해 Database 쓰기 작업 수행
+//       await queryRunner.commitTransaction(); // 트랜잭션 커밋.
+//     } catch (error) {
+//       console.error(ServiceMessage.__COMMAND_EXEC_FAILED, error);
+//       throw error;
+//     } finally {
+//       await queryRunner.release(); // 트랜잭션 해제
+//     }
+//   }
+// }
--- packages/api/src/Sample/domain/factory/sample.factory.ts
+++ packages/api/src/Sample/domain/factory/sample.factory.ts
@@ -1,52 +1,52 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { EventPublisher } from '@nestjs/cqrs';
-import { Sample } from '../entity/sample';
-import { Id } from '../value-object/id.vo';
-import { Password } from '../value-object/password.vo';
-import { Title } from '../value-object/title.vo';
-import { Content } from '../value-object/content.vo';
-import { Pokedex } from '../value-object/pokedex.vo';
-import { Ability } from '../value-object/ability.vo';
-import { Item } from '../value-object/item.vo';
-import { Tera } from '../value-object/tera.vo';
-import { IVs } from '../value-object/ivs.vo';
-import { EVs } from '../value-object/evs.vo';
-import { PartyTag } from '../value-object/partyTag.vo';
-import { SampleTag } from '../value-object/sampleTag.vo';
-import CreateSampleOptions from 'src/shared/type/sampleOptions.interface';
-import { Moves } from '../value-object/moves.vo';
-import { Nature } from '../value-object/nature.vo';
-import { SampleEntity } from 'src/sample/infrastructure/entity/sample.entity';
+// import { Sample } from '../entity/sample';
+// import { Id } from '../value-object/id.vo';
+// import { Password } from '../value-object/password.vo';
+// import { Title } from '../value-object/title.vo';
+// import { Content } from '../value-object/content.vo';
+// import { Pokedex } from '../value-object/pokedex.vo';
+// import { Ability } from '../value-object/ability.vo';
+// import { Item } from '../value-object/item.vo';
+// import { Tera } from '../value-object/tera.vo';
+// import { IVs } from '../value-object/ivs.vo';
+// import { EVs } from '../value-object/evs.vo';
+// import { PartyTag } from '../value-object/partyTag.vo';
+// import { SampleTag } from '../value-object/sampleTag.vo';
+// import CreateSampleOptions from 'src/shared/type/sampleOptions.interface';
+// import { Moves } from '../value-object/moves.vo';
+// import { Nature } from '../value-object/nature.vo';
+// import { SampleEntity } from 'src/sample/infrastructure/entity/sample.entity';
 @Injectable()
 export class SampleFactory {
   @Inject(EventPublisher) private readonly eventPublisher: EventPublisher;
 
-  create(options: CreateSampleOptions | SampleEntity): Sample {
-    const sample = new Sample(
-      options.index, // SampleId는 null로 시작함 (자동 생성됨)
-      Id.create(options.id),
-      Password.create(options.password),
-      Pokedex.create(options.pokedex),
-      Title.create(options.title),
-      Content.create(options.content),
-      Ability.create(options.ability),
-      Item.create(options.item),
-      Tera.create(options.tera),
-      IVs.create(options.ivs),
-      EVs.create(options.evs),
-      PartyTag.create(options.party_tag),
-      SampleTag.create(options.sample_tag),
-      Moves.create(options.moves),
-      Nature.create(options.nature),
-    );
+  // create(options: CreateSampleOptions | SampleEntity): Sample {
+  //   const sample = new Sample(
+  //     options.sample_index, // SampleId는 null로 시작함 (자동 생성됨)
+  //     Id.create(options.id),
+  //     Password.create(options.password),
+  //     Pokedex.create(options.pokedex),
+  //     Title.create(options.title),
+  //     Content.create(options.content),
+  //     Ability.create(options.ability),
+  //     Item.create(options.item),
+  //     Tera.create(options.tera),
+  //     IVs.create(options.ivs),
+  //     EVs.create(options.evs),
+  //     PartyTag.create(options.party_tag),
+  //     SampleTag.create(options.sample_tag),
+  //     Moves.create(options.moves),
+  //     Nature.create(options.nature),
+  //   );
 
-    // EventPublisher로 객체를 래핑하여 이벤트를 발행할 수 있도록 설정
-    const sampleWithEventPublisher =
-      this.eventPublisher.mergeObjectContext(sample);
+  //   // EventPublisher로 객체를 래핑하여 이벤트를 발행할 수 있도록 설정
+  //   const sampleWithEventPublisher =
+  //     this.eventPublisher.mergeObjectContext(sample);
 
-    // 필요한 초기 이벤트가 있다면 발행 가능
-    // sampleWithEventPublisher.apply(new SampleCreatedEvent(sampleWithEventPublisher));
+  //   // 필요한 초기 이벤트가 있다면 발행 가능
+  //   // sampleWithEventPublisher.apply(new SampleCreatedEvent(sampleWithEventPublisher));
 
-    return sampleWithEventPublisher;
-  }
+  //   return sampleWithEventPublisher;
+  // }
 }
