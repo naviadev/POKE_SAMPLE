50a780ed7955ebb84f8e6270082adb44e9cd2c13
naviadev <qo2oq1020@gmail.com>
2024-10-14 10:22:36 +0900

    Markup | topNavigator.tsx : 반응형 UI를 위해 스타일 수정

--- packages/client/src/components/organism/topNavigator/topNavigator.tsx
+++ packages/client/src/components/organism/topNavigator/topNavigator.tsx
@@ -1,37 +1,34 @@
-"use client"
+"use client";
 import MenuButton from "@/components/molecule/menuButton/menuButton";
 import Logo from "@/components/molecule/logo/logo";
 
 const TopNavigator = () => {
   const menuList = [
-    { text: "샘플", href: "/sample" }, // "샘플" 버튼을 "/"로 이동
+    { text: "샘플", href: "/sample" },
     { text: "파티", href: "/party" },
     { text: "계산기", href: "/calculator" },
   ];
 
   return (
-    <header className="sticky top-0 bg-white flex justify-center items-center p-4 border-b w-screen z-50">
-      <div className="flex justify-between w-11/12">
-        {/* 왼쪽 메뉴 */}
-        <div className="flex space-x-4">
-          {/* 임의의 Logo DIV */}
+    <header className="sticky top-0 bg-white flex justify-center items-center p-4 border-b w-full z-50">
+      <div className="flex flex-col sm:flex-row justify-between w-full max-w-7xl px-4">
+        <div className="flex justify-between items-center w-full sm:w-auto space-x-4">
           <Logo />
-          {/* menuList 동적 생성 */}
-          {menuList.map(({ text, href }) => (
-            <MenuButton
-              key={text}
-              className="w-24 h-10"
-              text={text}
-              href={href}
-            />
-          ))}
+          <div className="flex space-x-2 sm:space-x-4">
+            {menuList.map(({ text, href }) => (
+              <MenuButton
+                key={text}
+                className="w-16 sm:w-20 h-8 sm:h-10"
+                text={text}
+                href={href}
+              />
+            ))}
+          </div>
         </div>
 
-        {/* 오른쪽 메뉴 */}
-        <div className="flex space-x-4">
-          {/* 임시 메뉴 */}
-          <div className="w-20 h-10 bg-gray-300 rounded"></div>
-          <div className="w-20 h-10 bg-gray-300 rounded"></div>
+        <div className="flex justify-between items-center space-x-2 sm:space-x-4 mt-2 sm:mt-0">
+          <div className="w-16 sm:w-20 h-8 sm:h-10 bg-gray-300 rounded"></div>
+          <div className="w-16 sm:w-20 h-8 sm:h-10 bg-gray-300 rounded"></div>
         </div>
       </div>
     </header>
