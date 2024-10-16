4ae8d77edc3e77219f131b450cd535ef0550f6a1
naviadev <qo2oq1020@gmail.com>
2024-10-16 11:28:06 +0900

    Feat | moves.*.ts : moves 도메인을 다루는 엔드포인트 추가

--- packages/api/src/moves/presentation/controller/moves.query.controller.ts
+++ packages/api/src/moves/presentation/controller/moves.query.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
-import { MovesRepository } from 'src/moves/infrastructure/repository/moves.repository';
+import { Controller, Get, Param } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
+import { GetSearchMovesQuery } from 'src/moves/application/query/getSearchMoves.query';
+import { MOVES_RESPONSE_MASSAGE } from 'src/moves/enum/movesResponse.message.enum';
+import { withTryCatch } from 'src/shared/wrapper/tryCatch';
 
 @Controller('/moves/query')
 export class MovesQueryController {
-  constructor(private readonly movesRepository: MovesRepository) {}
+  constructor(private readonly queryBus: QueryBus) {}
+
+  @Get('/search/:value')
+  async GetSearchMoves(@Param('value') value: string) {
+    return await withTryCatch(async () => {
+      return this.queryBus.execute(new GetSearchMovesQuery(value));
+    }, MOVES_RESPONSE_MASSAGE.SEARCH_ERROR);
+  }
 }
