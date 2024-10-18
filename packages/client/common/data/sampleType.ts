b49679e93ba1f685c66d2f493d3e801209bf79df
naviadev <qo2oq1020@gmail.com>
2024-10-18 15:09:23 +0900

    Fix | sampleType.ts : value, label값 재정의

--- packages/client/common/data/sampleType.ts
+++ packages/client/common/data/sampleType.ts
@@ -1,16 +1,19 @@
 import Option from "../interface/option.interface"
+
 const sampleTypeOptions: Option[] = [
-  { label: "특수 어태커", value: 0 },
-  { label: "물리 어태커", value: 1 },
-  { label: "물리막이", value: 2 },
-  { label: "특수막이", value: 3 },
-  { label: "딜탱", value: 4 },
-  { label: "스카프", value: 5 },
-  { label: "기점", value: 6 },
-  { label: "변칙", value: 7 },
-  { label: "스위퍼", value: 8 },
-  { label: "선봉", value: 9 },
-  { label: "서포터", value: 10 }
+  { label: '특수 어태커', value: 1 },
+  { label: '물리 어태커', value: 2 },
+  { label: '물리 막이', value: 3 },
+  { label: '특수 막이', value: 4 },
+  { label: '딜탱', value: 5 },
+  { label: '기점', value: 6 },
+  { label: '선봉', value: 7 },
+  { label: '스카프', value: 8 },
+  { label: '스위퍼', value: 9 },
+  { label: '변칙', value: 10 },
+  { label: '서포터', value: 11 }
 ];
+
+
 
 export default sampleTypeOptions;
