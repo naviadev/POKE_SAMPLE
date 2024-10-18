449d9050428864193deffa5aa1f27938178b1f3d
naviadev <qo2oq1020@gmail.com>
2024-10-18 15:07:34 +0900

    Fix | nature.ts : 성격값 value, label 재설정

--- packages/client/common/data/nature.ts
+++ packages/client/common/data/nature.ts
@@ -1,31 +1,29 @@
 import Option from "../interface/option.interface";
-
 const pokemonNaturesOptions: Option[] = [
-  { label: "노력", value: 0 },     // Hardy
-  { label: "외로움", value: 1 },   // Lonely
-  { label: "고집", value: 2 },     // Adamant
-  { label: "개구쟁이", value: 3 }, // Naughty
-  { label: "용감", value: 4 },     // Brave
-  { label: "대담", value: 5 },     // Bold
-  { label: "온순", value: 6 },     // Docile
-  { label: "장난꾸러기", value: 7 }, // Impish
-  { label: "촐랑", value: 8 },     // Lax
-  { label: "무사태평", value: 9 }, // Relaxed
-  { label: "조심", value: 10 },    // Modest
-  { label: "의젓", value: 11 },    // Mild
-  { label: "수줍음", value: 12 },  // Bashful
-  { label: "덜렁", value: 13 },    // Rash
-  { label: "냉정", value: 14 },    // Quiet
-  { label: "차분", value: 15 },    // Calm
-  { label: "얌전", value: 16 },    // Gentle
-  { label: "신중", value: 17 },    // Careful
-  { label: "변덕", value: 18 },    // Quirky
-  { label: "건방", value: 19 },    // Sassy
-  { label: "겁쟁이", value: 20 },  // Timid
-  { label: "성급", value: 21 },    // Hasty
-  { label: "명랑", value: 22 },    // Jolly
-  { label: "천진난만", value: 23 }, // Naive
-  { label: "성실", value: 24 }     // Serious
+  { label: '외로움', value: 1 },
+  { label: '고집', value: 2 },
+  { label: '개구쟁이', value: 3 },
+  { label: '용감', value: 4 },
+  { label: '대담', value: 5 },
+  { label: '온순', value: 6 },
+  { label: '장난꾸러기', value: 7 },
+  { label: '촐랑', value: 8 },
+  { label: '무사태평', value: 9 },
+  { label: '조심', value: 10 },
+  { label: '의젓', value: 11 },
+  { label: '수줍음', value: 12 },
+  { label: '덜렁', value: 13 },
+  { label: '냉정', value: 14 },
+  { label: '차분', value: 15 },
+  { label: '얌전', value: 16 },
+  { label: '신중', value: 17 },
+  { label: '변덕', value: 18 },
+  { label: '건방', value: 19 },
+  { label: '겁쟁이', value: 20 },
+  { label: '성급', value: 21 },
+  { label: '명랑', value: 22 },
+  { label: '천진난만', value: 23 },
+  { label: '성실', value: 24 }
 ];
 
 export default pokemonNaturesOptions;
