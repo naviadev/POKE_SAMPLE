2188eb67c805d27a584ed506f8337850998f9244
naviadev <qo2oq1020@gmail.com>
2024-10-18 15:21:40 +0900

    Markup | logo.tsx : 임시 로고 작성

--- packages/client/src/components/molecule/logo/logo.tsx
+++ packages/client/src/components/molecule/logo/logo.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 const Logo = () => {
   return (
-    <h1>
+    <h1 className="font-bold text-typeColors-15 text-2xl">
       {/* <Image src="로고 경로 추후 수정" alt="Pokesample Logo" /> */}
-      <a href="/">로고</a>
+      <a href="/">POKESAMPLE</a>
     </h1>
   );
 };
